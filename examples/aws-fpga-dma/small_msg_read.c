5bee0b0fac2228a5c872c1ce6c2c50b332234fb3
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-06-02 21:47:58 -0400

    Trying read first of small message loopback

---
+++ examples/aws-fpga-dma/small_msg_read.c
@@ -0,0 +1,60 @@
+#include "aws_fpga_dma.c"
+
+#define SMALL 16  // BUFSIZE = 128,  DMA_MSG_SIZE 256
+dma_msg_t small_msg;
+uint1_t small_msg_valid = 1;
+
+#pragma MAIN_MHZ main_wrapper 150.0
+void main_wrapper(uint1_t rst)
+{
+	// Read ready for output msg flag from aws_fpga_dma
+  uint1_t_array_1_t out_readys;
+  out_readys = aws_out_msg_ready_READ();
+  uint1_t out_ready;
+  out_ready = out_readys.data[0];
+
+	// Read DMA message bytes from aws_fpga_dma
+  dma_msg_s_array_1_t msgs_in;
+  msgs_in = aws_in_msg_READ();
+  dma_msg_s msg_in;
+  msg_in = msgs_in.data[0];
+  
+  // Write DMA message bytes into aws_fpga_dma
+	dma_msg_s_array_1_t msgs_out;
+	msgs_out.data[0].data = small_msg; // Small message
+  msgs_out.data[0].valid = small_msg_valid; // Small message
+	aws_out_msg_WRITE(msgs_out);
+  
+  // Clear valid if was ready for msg
+  if(out_ready)
+  {
+    small_msg_valid = 0;
+  }
+  
+  // Ready for input message if dont have valid msg
+  // Ready for input message if dont have valid msg
+  // Accept input message if current message invalid
+  uint1_t msg_in_ready = !small_msg_valid;
+  if(msg_in_ready)
+  {
+    // Small is only 16 bytes in example
+    uint8_t i;
+    for(i=0;i<SMALL;i=i+1)
+    {
+      small_msg.data[i] = msg_in.data.data[i];
+    }
+    small_msg_valid = msg_in.valid;
+  }
+  
+  // Write ready for input message indicator into aws_fpga_dma
+  uint1_t_array_1_t in_readys;
+	in_readys.data[0] = msg_in_ready; 
+	aws_in_msg_ready_WRITE(in_readys);
+  
+  // Reset revalidates message of zeros to read
+  if(rst)
+  {
+    small_msg = DMA_MSG_T_NULL(); // Zeros data
+    small_msg_valid = 1;
+  }
+}
---
+++ examples/aws-fpga-dma/small_msg_read_test.c
@@ -0,0 +1,70 @@
+// This is the main program for driving a small msg read test
+#define SMALL 16
+
+#include <math.h>
+#include <time.h>
+
+#include "dma_msg_sw.c"
+#include "work_sw.c"
+
+dma_msg_t inc_read_msg(dma_msg_t msg)
+{
+  // Zero out
+  for(int v=0;v<DMA_MSG_SIZE;v++)
+	{
+		msg.data[v] = 0;
+	}
+  // Increment small part of message
+  for(int v=0;v<SMALL;v++)
+	{
+		msg.data[v] = msg.data[v] + 1;
+	}
+	return msg;
+}
+
+// Helper to compare two output datas
+int compare_bad(int i, dma_msg_t expected, dma_msg_t fpga)
+{
+  int bad = 0;
+  for(int v=0;v<DMA_MSG_SIZE;v++)
+	{
+		if(fpga.data[v] != expected.data[v])
+    {
+      bad = 1;
+      printf("Output %d does not match at %d! FPGA: %d, EXPECTED: %d\n", i, v, fpga.data[v], expected.data[v]);
+      break;
+    }
+	}
+	return bad;
+}
+
+// Init + do some work + close
+int main(int argc, char **argv) 
+{
+	// Init direct memory access to/from FPGA
+	init_dma();
+	
+	// Do something using the function 'work_fpga()'
+	// which uses the FPGA to compute the 'work()' function
+  dma_msg_t expected_read_msg = DMA_MSG_T_NULL();
+  
+  // Loop of reading message and writing it back modified
+  int n = 10000;
+  for(int i = 0; i < n; i++)
+	{
+		// Read
+    dma_msg_t read_msg;
+    read_msg = dma_read();
+    // Compare
+    if(compare_bad(i,expected_read_msg,read_msg))
+		{
+			break;
+		}
+    // Write modified msg
+    expected_read_msg = inc_read_msg(read_msg);
+    dma_write(expected_read_msg);
+	}
+
+	// Close direct memory access to/from FPGA
+	close_dma();    
+}
