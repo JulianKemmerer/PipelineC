7e8b52a9acb65cff0b34ea026e5f7aa78d5ece95
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-06-06 23:28:05 -0400

    Dont need imports unless used never anyway

--- src/C_TO_LOGIC.py
+++ src/C_TO_LOGIC.py
@@ -4,9 +4,6 @@ import os
 from pycparser import c_parser, c_ast, c_generator
 import copy
 import pickle
-import matplotlib.pyplot as plt
-import networkx as nx
-import numpy as np
 import math
 import hashlib
 import shlex
@@ -1084,6 +1081,9 @@ class Logic:
 # Thanks stack overflow
 #https://stackoverflow.com/questions/29572623/plot-networkx-graph-from-adjacency-matrix-in-csv-file
 def show_graph_with_labels(adjacency_matrix, mylabels):
+    import matplotlib.pyplot as plt
+    import networkx as nx
+    import numpy as np
     rows, cols = np.where(adjacency_matrix == 1)
     edges = zip(rows.tolist(), cols.tolist())
     gr = nx.Graph()
