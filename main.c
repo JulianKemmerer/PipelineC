c4b5a6e5d6daddd89019df3745615ed42c3b0daa
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-06-12 14:32:08 -0400

    Software bug in host fixed hello world loop trying bram loop again

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 
 //#include "examples/aws-fpga-dma/loopback.c"
-#include "examples/fosix/hello_world.c"
-//#include "examples/fosix/bram_loopback.c"
+//#include "examples/fosix/hello_world.c"
+#include "examples/fosix/bram_loopback.c"
 
+/*
+#pragma MAIN_MARK_DEBUG main
+#pragma MAIN_MHZ main 150.0
+float main(float x, float y)
+{
+  return x+y;
+}
+*/
