d5ff7016421ae87717ffb51f32420e242a803bbd
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-06-27 14:18:46 -0400

    Mildly useful fifo macro

---
+++ fifo.h
@@ -0,0 +1,63 @@
+#pragma once
+#include "uintN_t.h"
+// Macros for fifos
+
+// A simple, low throughput, scalable in registers, fwft, shifting fifo
+#define fifo_shift(name, type, size) \
+type name##_data_buf[size];\
+uint1_t name##_valid_buf[size];\
+/* Wish C could return tuples...*/\
+typedef struct name##_t\
+{\
+  type data_out;\
+  uint1_t data_out_valid;\
+  uint1_t data_in_ready;\
+  uint1_t write_ack;\
+} name##_t;\
+name##_t name##_func(uint1_t rd, type data_in, uint1_t wr)\
+{\
+  /* Shift buffer thing, shift an element forward if possible*/\
+  /* as elements are read out from front*/\
+  name##_t rv;\
+  /* Read from front, fwft */\
+  rv.data_out = name##_data_buf[0];\
+  rv.data_out_valid = name##_valid_buf[0];\
+  /* Read clears output buffer to allow for shifting forward*/\
+  if(rd)\
+  {\
+    name##_valid_buf[0] = 0;\
+  }\
+  \
+  /* Shift array elements forward if possible*/\
+  /* Which positions should shift?*/\
+  uint1_t pos_do_shift[size];\
+  pos_do_shift[0] = 0; /* Nothing ot left of 0*/\
+  uint64_t i;\
+  for(i=1;i<size;i=i+1)\
+  {\
+    /* Can shift to next pos/left if empty */\
+    pos_do_shift[i] = !name##_valid_buf[i-1];\
+  }\
+  /* Do the shifting*/\
+  for(i=1;i<size;i=i+1)\
+  {\
+    if(pos_do_shift[i])\
+    {\
+      /* Shift to left, and invalidate source on right */\
+      name##_data_buf[i-1] = name##_data_buf[i];\
+      name##_valid_buf[i-1] = name##_valid_buf[i];\
+      name##_valid_buf[i] = 0;\
+    }\
+  }\
+  /* Signal if ready for input */\
+  rv.data_in_ready = !name##_valid_buf[size-1];\
+  /* Input write goes into back of queue if ready*/\
+  rv.write_ack = 0;\
+  if(rv.data_in_ready)\
+  {\
+    name##_data_buf[size-1] = data_in;\
+    name##_valid_buf[size-1] = wr;\
+    rv.write_ack = wr;\
+  }\
+  return rv;\
+}
--- main.c
+++ main.c
@@ -1,30 +1,23 @@
 
 //#include "examples/aws-fpga-dma/loopback.c"
-#include "examples/aws-fpga-dma/work.c"
+//#include "examples/aws-fpga-dma/work.c"
 //#include "examples/fosix/hello_world.c"
 //#include "examples/fosix/bram_loopback.c"
 
-/*
-#include "uintN_t.h"
-typedef struct point
+#include "fifo.h"
+
+typedef struct some_type
 {
   uint8_t an_array[3][3];
   float a_float;
   uint64_t a_u64;
-} point;
-// TODO gen all inside point_bytes_t.h
-#include "uint8_t_array_N_t.h" 
-#include "uint8_t_bytes_t.h"
-#include "float_bytes_t.h"
-#include "uint64_t_bytes_t.h"
-#include "point_bytes_t.h"
+} some_type;
+
+// A fifo 
+fifo_shift(the_fifo, some_type, 32)
 
 #pragma MAIN_MHZ main 150.0
-
-point main(point p)
+the_fifo_t main(uint1_t read, some_type x, uint1_t write)
 {
-  point_bytes_t bytes = point_to_bytes(p);
-  point out_p = bytes_to_point(bytes);
-  return out_p;
+  return the_fifo_func(read, x, write);
 }
-*/
