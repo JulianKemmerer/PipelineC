cc920fdea459a051ad390c7b534b2ae468c5e1c6
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-07-04 17:51:48 -0400

    Building loopback from mt testing

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 
-//#include "examples/aws-fpga-dma/loopback.c"
-#include "examples/aws-fpga-dma/work.c"
+#include "examples/aws-fpga-dma/loopback.c"
+//#include "examples/aws-fpga-dma/work.c"
 //#include "examples/fosix/hello_world.c"
 //#include "examples/fosix/bram_loopback.c"
 
