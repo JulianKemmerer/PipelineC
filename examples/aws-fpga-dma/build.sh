79e6c1d0ee3ee9b904061877703ea48ba0d108a0
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-07-05 13:22:23 -0400

    Building aws at 250 now

--- examples/aws-fpga-dma/build.sh
+++ examples/aws-fpga-dma/build.sh
@@ -7,4 +7,6 @@ python -u ./src/main.py 2>&1
 # Build dcp that will be turned into AFI
 echo "Beginning AWS build process..."
 cd $CL_DIR/build/scripts
-./aws_build_dcp_from_cl.sh
+#./aws_build_dcp_from_cl.sh
+# In order to use a 250 MHz main clock the developer can specify the A1 Clock Group A Recipe
+./aws_build_dcp_from_cl.sh -clock_recipe_a A1
