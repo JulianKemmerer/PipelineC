484cc1b9b92b35ea2130551772a0c440021daee4
JulianKemmerer <julian.v.kemmerer@gmail.com>
2020-09-25 18:29:35 -0400

    Preparing for lattice support

--- src/main.py
+++ src/main.py
@@ -21,6 +21,8 @@ print('''
 ''')
 
 print("TODO:")
+print(" Add Lattice support?")
+print(" Static locals == globals as an option, better for scope AND globals cant be used by multiple funcs right now so...")
 print(" Document pragmas and make main mhz allow pointing at existing func name instead of float")
 print(" Do a better Arty7 example - hobby more likely to try something new?")
 print(" Do a simple RISCV? cpu? But avoid just doing RTL clone?")
