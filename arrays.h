d798abf2569cc418bc3b88372c12f419ab5e943a
JulianKemmerer <julian.v.kemmerer@gmail.com>
2021-03-30 20:33:59 -0400

    Updating arrays.h and some path delays

--- arrays.h
+++ arrays.h
@@ -1,3 +1,19 @@
+
+#define ARRAY_COPY(dest,src,n)\
+uint32_t ARRAY_COPY_i; \
+for(ARRAY_COPY_i=0;ARRAY_COPY_i<n;ARRAY_COPY_i+=1) \
+{ \
+  dest[ARRAY_COPY_i] = src[ARRAY_COPY_i]; \
+}
+
+#define ARRAY_SET(dest,val,n)\
+uint32_t ARRAY_SET_i; \
+for(ARRAY_SET_i=0;ARRAY_SET_i<n;ARRAY_SET_i+=1) \
+{ \
+  dest[ARRAY_SET_i] = val; \
+}
+
+// TODO make these not need 'i' and define extra ARRAY_SHIFT_DOWN_i in-line
 #define ARRAY_SHIFT_DOWN(a,SIZE,AMOUNT,i)\
 for(i=AMOUNT;i<SIZE;i+=1) \
 { \
