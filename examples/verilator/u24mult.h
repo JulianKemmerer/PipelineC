a8dd3dd699441d9b9a2f4f327f920a3aabd895b4
JulianKemmerer <julian.v.kemmerer@gmail.com>
2021-09-30 22:57:10 -0400

    Use cxxflags to fix hard coded include paths in verilator build

--- examples/verilator/u24mult.h
+++ examples/verilator/u24mult.h
@@ -1,6 +1,6 @@
 // TODO: How to compile these without fixed paths? Tried -I{dir}?
 // uintN_t types for any N
-#include "/media/1TB/Dropbox/PipelineC/git/PipelineC/uintN_t.h"  
+#include "uintN_t.h"  
 
 uint48_t u24mult(uint24_t x, uint24_t y)
 {
