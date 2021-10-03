85c7bcace91b48b52714e0c118a05578c26d9888
JulianKemmerer <julian.v.kemmerer@gmail.com>
2021-10-03 14:41:54 -0400

    Trying to match working sqrtf example

--- examples/llvm/rsqrtf.c
+++ examples/llvm/rsqrtf.c
@@ -7,13 +7,15 @@
 #define BITCAST_I32(x) float_31_0(x)
 #define BITCAST_FLOAT(x) float_uint32(x)
 
-#pragma PART "EP4CE22F17C6"
+#define MUL(a, b) ((a)*(b)) //may use instead a primitive optimized for using FPGA multipliers
+
+//#pragma PART "EP4CE22F17C6"
 
 // Temp manually code up multiplier hard coded with Intel mult prims
-#include "primitives/cyclone_iv.c"
+//#include "primitives/cyclone_iv.c"
 
 //#pragma MAIN_MHZ llvm_dis_Z11float_rsqrtf 250.0
-#pragma MAIN llvm_dis_Z11float_rsqrtf
+//#pragma MAIN llvm_dis_Z11float_rsqrtf
 float llvm_dis_Z11float_rsqrtf( FLOAT a0)
 {
   FLOAT a2;
@@ -29,18 +31,25 @@ float llvm_dis_Z11float_rsqrtf( FLOAT a0)
   FLOAT a12;
   FLOAT a13;
   a2 = LOAD(llvm_dis_float_rsqrt_K0); // %2 = load float, float* @float_rsqrt_K0, align 4, !tbaa !3
-  a3 = cyclone_iv_fp_mult(a2, a0); //a2 * a0; // %3 = fmul float %2, %0
+  a3 = MUL(a2, a0); //a2 * a0; // %3 = fmul float %2, %0
   a4 = LOAD(llvm_dis_float_rsqrt_K1); // %4 = load float, float* @float_rsqrt_K1, align 4, !tbaa !3
   a5 = BITCAST_I32(a0); // %5 = bitcast float %0 to i32
   a6 = LOAD(llvm_dis_float_rsqrt_K2); // %6 = load i32, i32* @float_rsqrt_K2, align 4, !tbaa !7
   a7 = a5 >> 1; // %7 = lshr i32 %5, 1
   a8 = a6 - a7; // %8 = sub i32 %6, %7
   a9 = BITCAST_FLOAT(a8); // %9 = bitcast i32 %8 to float
-  a10 = cyclone_iv_fp_mult(a3, a9); //a3 * a9; // %10 = fmul float %3, %9
-  a11 = cyclone_iv_fp_mult(a10, a9); //a10 * a9; // %11 = fmul float %10, %9
+  a10 = MUL(a3, a9); //a3 * a9; // %10 = fmul float %3, %9
+  a11 = MUL(a10, a9); //a10 * a9; // %11 = fmul float %10, %9
   a12 = a4 - a11; // %12 = fsub float %4, %11
-  a13 = cyclone_iv_fp_mult(a12, a9); //a12 * a9; // %13 = fmul float %12, %9
+  a13 = MUL(a12, a9); //a12 * a9; // %13 = fmul float %12, %9
   return a13; // ret float %13
+}
+
+#pragma MAIN app
+float app()
+{
+  float x = 4.0;
+  return llvm_dis_Z11float_rsqrtf(x);
 }
 
 /*
