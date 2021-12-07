92fc1463cf2e7c7b968f2e60190bbd0ae3ed0cb9
JulianKemmerer <julian.v.kemmerer@gmail.com>
2021-12-06 20:56:29 -0500

    Updating basic mnist nn demo to be fsm style

--- examples/arty/src/mnist/rand_net_c_gen.py
+++ examples/arty/src/mnist/rand_net_c_gen.py
@@ -1,5 +1,21 @@
 import random
-for l in range(0,10):
-  print("network.b[",l,"] = ", random.uniform(0, 1), ";")
-  for i in range(0,(8*8)):
-    print("network.W[", l, "]", "[", i, "] = ", random.uniform(0, 1), ";")
+
+MNIST_IMAGE_WIDTH=28
+MNIST_IMAGE_HEIGHT=28
+MNIST_IMAGE_SIZE=MNIST_IMAGE_WIDTH * MNIST_IMAGE_HEIGHT
+MNIST_LABELS=10
+
+print("PIXELS:")
+for j in range(0,MNIST_IMAGE_SIZE):
+  print(int(random.uniform(0, 1)*255),",")
+print("")
+
+print("BIASES:")
+for i in range(0, MNIST_LABELS):
+  print(random.uniform(0, 1),",")
+print("")
+
+print("WEIGHTS:")
+for a in range(0,MNIST_LABELS*MNIST_IMAGE_SIZE):
+  print(random.uniform(0, 1),",")
+print("")
