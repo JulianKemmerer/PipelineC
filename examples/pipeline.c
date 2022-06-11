320bd57a1efffc21323578490d93a264fad45df4
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-06-10 22:13:46 -0400

    Cleaning up example pipeline file

--- examples/pipeline.c
+++ examples/pipeline.c
@@ -1,22 +1,14 @@
 // A II=1 pipeline from a pure C function (no globals or local static vars)
 
 // Set FPGA part/synthesis tool see ../main.c for examples.
-#pragma PART "LFE5UM5G-85F-8BG756C"
+//#pragma PART "LFE5UM5G-85F-8BG756C"
 
 #include "intN_t.h"
 #include "uintN_t.h"
 
+//#pragma MAIN_MHZ my_pipeline 250.0
 #pragma MAIN my_pipeline
-uint64_t my_pipeline(uint64_t x, uint64_t y)
+float my_pipeline(float x, float y)
 {
-  uint64_t rv;
-  if(x > y)
-  {
-    rv = x + y;
-  }
-  else
-  {
-    rv = x * y;
-  }
-  return rv;
+  return x + y;
 }
