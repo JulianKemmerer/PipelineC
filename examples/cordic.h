bb53a127c9c705bb9e8d645a278cedfbad21f5b5
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-07-22 23:38:20 -0400

    Update cordic example

--- examples/cordic.h
+++ examples/cordic.h
@@ -1,8 +1,5 @@
-// Heys its math and more!
 #include "uintN_t.h"
 #include "intN_t.h"
-
-//TODO fake some of math.h #define M_PI 
 
 // http://www.dcs.gla.ac.uk/~jhw/cordic/     **Slightly modified
 //Cordic in 32 bit signed fixed point math
@@ -25,53 +22,18 @@ typedef struct cordic_fixed32_t
 	int32_t c;
 } cordic_fixed32_t;
 
+int32_t cordic_ctab[CORDIC_NTAB] = {0x3243F6A8, 0x1DAC6705, 0x0FADBAFC, 0x07F56EA6, 0x03FEAB76, 0x01FFD55B, 
+0x00FFFAAA, 0x007FFF55, 0x003FFFEA, 0x001FFFFD, 0x000FFFFF, 0x0007FFFF, 0x0003FFFF, 
+0x0001FFFF, 0x0000FFFF, 0x00007FFF, 0x00003FFF, 0x00001FFF, 0x00000FFF, 0x000007FF, 
+0x000003FF, 0x000001FF, 0x000000FF, 0x0000007F, 0x0000003F, 0x0000001F, 0x0000000F, 
+0x00000008, 0x00000004, 0x00000002, 0x00000001, 0x00000000};
+
 cordic_fixed32_t cordic_fixed32_n32(int32_t theta)//, int32_t *s, int32_t *c, int32_t n)
 {
-    int32_t cordic_ctab[CORDIC_NTAB];
-    cordic_ctab[0] = 0x3243F6A8;
-    cordic_ctab[1] = 0x1DAC6705;
-    cordic_ctab[2] = 0x0FADBAFC;
-    cordic_ctab[3] = 0x07F56EA6;
-    cordic_ctab[4] = 0x03FEAB76;
-    cordic_ctab[5] = 0x01FFD55B;
-    cordic_ctab[6] = 0x00FFFAAA;
-    cordic_ctab[7] = 0x007FFF55; 
-    cordic_ctab[8] = 0x003FFFEA; 
-    cordic_ctab[9] = 0x001FFFFD; 
-    cordic_ctab[10] = 0x000FFFFF; 
-    cordic_ctab[11] = 0x0007FFFF; 
-    cordic_ctab[12] = 0x0003FFFF; 
-	cordic_ctab[13] = 0x0001FFFF; 
-	cordic_ctab[14] = 0x0000FFFF; 
-	cordic_ctab[15] = 0x00007FFF; 
-	cordic_ctab[16] = 0x00003FFF; 
-	cordic_ctab[17] = 0x00001FFF; 
-	cordic_ctab[18] = 0x00000FFF; 
-	cordic_ctab[19] = 0x000007FF; 
-	cordic_ctab[20] = 0x000003FF; 
-	cordic_ctab[21] = 0x000001FF; 
-	cordic_ctab[22] = 0x000000FF; 
-	cordic_ctab[23] = 0x0000007F; 
-	cordic_ctab[24] = 0x0000003F; 
-	cordic_ctab[25] = 0x0000001F; 
-	cordic_ctab[26] = 0x0000000F; 
-	cordic_ctab[27] = 0x00000008; 
-	cordic_ctab[28] = 0x00000004; 
-	cordic_ctab[29] = 0x00000002; 
-	cordic_ctab[30] = 0x00000001; 
-	cordic_ctab[31] = 0x00000000;
-  
-  int32_t k;
-  int32_t d;
-  int32_t tx;
-  int32_t ty;
-  int32_t tz;
-  int32_t x;
-  x = CORDIC_1K;
-  int32_t y;
-  y = 0;
-  int32_t z;
-  z = theta;
+  int32_t k, d, tx, ty, tz;
+  int32_t x = CORDIC_1K;
+  int32_t y = 0;
+  int32_t z = theta;
   for (k=0; k<CORDIC_N_ITER; k=k+1)
   {
     d = z>>31;
@@ -99,20 +61,29 @@ typedef struct cordic_float_t
 cordic_float_t cordic_float_fixed32_n32(float theta)
 {
 	// Convert float to fixed
-	int32_t theta_fixed;
-	theta_fixed = theta*CORDIC_MUL;
+	int32_t theta_fixed = theta*CORDIC_MUL;
 	
 	// Do fixed math
-	cordic_fixed32_t fixed_result;
-	fixed_result = cordic_fixed32_n32(theta_fixed);
+	cordic_fixed32_t fixed_result = cordic_fixed32_n32(theta_fixed);
 
 	// Convert fixed to float
 	cordic_float_t rv;
-	rv.s = ((float)fixed_result.s)/CORDIC_MUL;
-	rv.c = ((float)fixed_result.c)/CORDIC_MUL;
+	rv.s = ((float)fixed_result.s)*(1.0/CORDIC_MUL);
+	rv.c = ((float)fixed_result.c)*(1.0/CORDIC_MUL);
 	return rv;
 }
 
+//#pragma MAIN_MHZ cordic_fixed32_n32 166.0
+//#pragma MAIN_MHZ cordic_float_fixed32_n32 166.0
+#pragma MAIN tb
+void tb()
+{
+  float rads = 1.0471975512; //~pi/3;
+  cordic_float_t result = cordic_float_fixed32_n32(rads);
+  printf("sin(%f)=%f, cos(%f)=%f\n", rads, result.s, rads, result.c);
+} 
+
+/*
 // Cordic float -inf - inf
 // TODO: Move modulo and adjustment for fixed point stuff into fixed point math
 //	via modulo 2PI then adjsut into range -pi/2 -- pi/2
@@ -209,10 +180,4 @@ float sin(float theta)
 	cordic_result = cordic_float_mod_fixed32_n32(theta);
 	return cordic_result.s;
 }
-
-// Placeholder for compile
-float sqrt(float x)
-{
-	return 0.0;
-}
-
+*/
