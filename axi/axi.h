a22aefe5b8de5ddce4c2edcb6f8908ea3e335b56
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-08-27 00:27:15 -0400

    Initial code for AXI example used for ddr3 memtest

--- axi/axi.h
+++ axi/axi.h
@@ -1,9 +1,295 @@
 #pragma once
 #include "uintN_t.h"
 
-#define axi_id_t uint6_t
+// https://developer.arm.com/documentation/102202/0300/Channel-signals
 
-// 512b AXI-4 bus is bidirectional
+#define axi_id_t uint16_t // Max size=16b?
+#define axi_addr_t uint64_t // Max size=64b?
+
+#define BURST_FIXED 0 // Fixed byte lanes only defined by start address and size.
+#define BURST_INCR  1 // Incrementing burst.
+#define BURST_WRAP  2 // If an upper address limit is reached, the address wraps around to a lower address.
+
+typedef struct axi32_write_req_t
+{
+  // Address
+  //  "Write this stream to a location in memory"
+  //  Typically valid before data starts?
+  axi_id_t awid;
+  axi_addr_t awaddr; 
+  uint8_t awlen; // In cycles
+  uint3_t awsize; // 2^size = Transfer width in bytes, 8 to 1024 bits supported
+  uint2_t awburst;
+  uint1_t awvalid;
+}axi32_write_req_t;
+
+typedef struct axi32_write_data_t
+{
+  // Data stream to be written to memory
+  uint8_t wdata[4]; // 4 bytes, 32b
+  uint1_t wstrb[4];
+  uint1_t wlast;
+  uint1_t wvalid;
+}axi32_write_data_t;
+
+typedef struct axi32_write_resp_t
+{
+  // Write response
+  axi_id_t bid;
+  uint2_t bresp; // Error code
+  uint1_t bvalid;
+} axi32_write_resp_t;
+
+typedef struct axi32_write_host_to_dev_t
+{
+  axi32_write_req_t req;
+  axi32_write_data_t data;
+  //  Read to receive write responses
+  uint1_t bready;
+} axi32_write_host_to_dev_t;
+
+typedef struct axi32_write_dev_to_host_t
+{
+  axi32_write_resp_t resp;
+  //  Ready for write address
+  uint1_t awready;
+	//  Ready for write data 
+  uint1_t wready;
+} axi32_write_dev_to_host_t;
+
+typedef struct axi32_read_req_t
+{
+  // Address
+  //   "Give me a stream from a place in memory"
+  //   Should be valid before data starts
+  axi_id_t arid;
+  axi_addr_t araddr;
+  uint8_t arlen;
+  uint3_t arsize; // 2^size = Transfer width in bytes, 8 to 1024 bits supported
+  uint2_t arburst;
+  uint1_t arvalid;
+} axi32_read_req_t;
+
+typedef struct axi32_read_resp_t
+{
+  // Read response
+  axi_id_t rid;
+  uint2_t rresp;
+} axi32_read_resp_t;
+
+typedef struct axi32_read_data_t
+{
+  // Data stream from memory
+  uint8_t rdata[4]; // 4 bytes, 32b
+  uint1_t rlast;
+  uint1_t rvalid;
+} axi32_read_data_t;
+
+typedef struct axi32_read_host_to_dev_t
+{
+  axi32_read_req_t req;
+  //  Ready to receive read data
+  uint1_t rready;
+} axi32_read_host_to_dev_t;
+
+typedef struct axi32_read_dev_to_host_t
+{
+  axi32_read_resp_t resp;
+  axi32_read_data_t data;
+  //  Ready for read address
+  uint1_t arready;
+} axi32_read_dev_to_host_t;
+
+typedef struct axi32_host_to_dev_t
+{
+  // Write Channel
+  axi32_write_host_to_dev_t write;
+  // Read Channel
+  axi32_read_host_to_dev_t read;
+} axi32_host_to_dev_t;
+
+typedef struct axi32_dev_to_host_t
+{
+  // Write Channel
+  axi32_write_dev_to_host_t write;
+  // Read Channel
+  axi32_read_dev_to_host_t read;
+} axi32_dev_to_host_t;
+
+typedef enum axi32_state_t
+{
+  REQ,
+  RESP,
+}axi32_state_t;
+
+typedef struct axi32_write_logic_outputs_t
+{
+  axi32_host_to_dev_t to_dev;
+  uint2_t bresp; // Response error code
+  uint1_t done;
+  uint1_t ready_for_inputs;
+}axi32_write_logic_outputs_t;
+axi32_write_logic_outputs_t axi32_write_logic(
+  uint32_t addr,
+  uint32_t data,
+  uint1_t ready_for_outputs,
+  axi32_dev_to_host_t from_dev
+){
+  static axi32_state_t state;
+  axi32_write_logic_outputs_t o;
+  if(state==REQ)
+  {
+    // Wait device to be ready for write request and data
+    if(from_dev.write.awready & from_dev.write.wready)
+    {
+      // Signal ready for inputs
+      o.ready_for_inputs = 1;
+      // Use inputs to form valid request
+      o.to_dev.write.req.awvalid = 1;
+      o.to_dev.write.req.awaddr = addr;
+      o.to_dev.write.req.awid = 0; // Just one constant AXI ID for now
+      o.to_dev.write.req.awlen = 1; // 1 transfer (non-burst)
+      o.to_dev.write.req.awsize = 2; // 2^2=4 bytes per transfer
+      o.to_dev.write.req.awburst = BURST_FIXED; // Not a burst, single fixed address per transfer 
+      o.to_dev.write.data.wvalid = 1;
+      // All 4 bytes are being transfered (uint to array)
+      uint32_t i;
+      for(i=0; i<4; i+=1)
+      {
+        o.to_dev.write.data.wdata[i] = data >> (i*8);
+        o.to_dev.write.data.wstrb[i] = 1;
+      }
+      o.to_dev.write.data.wlast = 1; // 1 transfer is last one
+      // And then begin waiting for response
+      state = RESP;
+    }
+  }
+  else if(state==RESP)
+  {
+    // Wait for this function to be ready for output
+    if(ready_for_outputs)
+    {
+      // Then signal to device that ready for response
+      o.to_dev.write.bready = 1;
+      // And wait for valid output response
+      if(from_dev.write.resp.bvalid)
+      {
+        // Done (error code not checked, just returned)
+        o.done = 1;
+        o.bresp = from_dev.write.resp.bresp;
+        state = REQ;
+      }
+    }
+  }
+  return o;
+}
+
+typedef struct axi32_read_logic_outputs_t
+{
+  axi32_host_to_dev_t to_dev;
+  uint32_t data; // Output data
+  uint2_t rresp; // Response error code
+  uint1_t done;
+  uint1_t ready_for_inputs;
+}axi32_read_logic_outputs_t;
+axi32_read_logic_outputs_t axi32_read_logic(
+  uint32_t addr,
+  uint1_t ready_for_outputs,
+  axi32_dev_to_host_t from_dev
+){
+  static axi32_state_t state;
+  axi32_read_logic_outputs_t o;
+  if(state==REQ)
+  {
+    // Wait device to be ready for request inputs
+    if(from_dev.read.arready)
+    {
+      // Signal function is ready for inputs
+      o.ready_for_inputs = 1;
+      // Use inputs to form valid request
+      o.to_dev.read.req.arvalid = 1;
+      o.to_dev.read.req.araddr = addr;
+      o.to_dev.read.req.arid = 0; // Just one constant AXI ID for now
+      o.to_dev.read.req.arlen = 1; // 1 transfer (non-burst)
+      o.to_dev.read.req.arsize = 2; // 2^2=4 bytes per transfer
+      o.to_dev.read.req.arburst = BURST_FIXED; // Not a burst, single fixed address per transfer
+      // And then begin waiting for response
+      state = RESP;
+    }
+  }
+  else if(state==RESP)
+  {
+    // Wait for this function to be ready for output
+    if(ready_for_outputs)
+    {
+      // Then signal to device that ready for response
+      o.to_dev.read.rready = 1;
+      // And wait for valid output response
+      if(from_dev.read.data.rvalid)
+      {
+        // Done
+        // Assumed last, error code not checked, just returned
+        o.done = 1;
+        o.data = uint8_array4_le(from_dev.read.data.rdata); // Array to uint
+        o.rresp = from_dev.read.resp.rresp;
+        state = REQ;
+      }
+    }
+  }
+  return o;
+}
+
+
+/*
+typedef struct axi512_write_resp_t
+{
+  //  Write response
+  axi_id_t bid;
+  uint2_t bresp; // Error code
+  uint1_t bvalid;
+} axi512_write_resp_t;
+
+typedef struct axi512_write_ready_t
+{
+  //  Ready for write address
+  uint1_t awready;
+	//  Ready for write data 
+  uint1_t wready;
+} axi512_write_ready_t;
+
+typedef struct axi512_write_o_t
+{
+  axi512_write_resp_t resp;
+  axi512_write_ready_t ready;
+} axi512_write_o_t;
+
+typedef struct axi512_read_resp_t
+{
+  //  Data stream from memory
+  axi_id_t rid;
+  uint8_t rdata[64];
+  uint2_t rresp;
+  uint1_t rlast;
+  uint1_t rvalid;
+} axi512_read_resp_t;
+
+typedef struct axi512_read_o_t
+{
+  axi512_read_resp_t resp;
+  //  Ready for read address
+  uint1_t arready;
+} axi512_read_o_t;
+
+//	Outputs
+typedef struct axi512_o_t
+{
+  // Write Channel
+  axi512_write_o_t write;
+  
+  // Read Channel
+  axi512_read_o_t read;
+  
+} axi512_o_t;
 
 typedef struct axi512_write_req_t
 {
@@ -58,55 +344,4 @@ typedef struct axi512_i_t
   axi512_read_i_t read;
   
 } axi512_i_t;
-
-
-typedef struct axi512_write_resp_t
-{
-  //  Write response
-  axi_id_t bid;
-  uint2_t bresp; // Error code
-  uint1_t bvalid;
-} axi512_write_resp_t;
-
-typedef struct axi512_write_ready_t
-{
-  //  Ready for write address
-  uint1_t awready;
-	//  Ready for write data 
-  uint1_t wready;
-} axi512_write_ready_t;
-
-typedef struct axi512_write_o_t
-{
-  axi512_write_resp_t resp;
-  axi512_write_ready_t ready;
-} axi512_write_o_t;
-
-typedef struct axi512_read_resp_t
-{
-  //  Data stream from memory
-  axi_id_t rid;
-  uint8_t rdata[64];
-  uint2_t rresp;
-  uint1_t rlast;
-  uint1_t rvalid;
-} axi512_read_resp_t;
-
-typedef struct axi512_read_o_t
-{
-  axi512_read_resp_t resp;
-  //  Ready for read address
-  uint1_t arready;
-} axi512_read_o_t;
-
-//	Outputs
-typedef struct axi512_o_t
-{
-  // Write Channel
-  axi512_write_o_t write;
-  
-  // Read Channel
-  axi512_read_o_t read;
-  
-} axi512_o_t;
-
+*/
---
+++ examples/axi/axi_ddr_host.c
@@ -0,0 +1,125 @@
+#include "uintN_t.h"
+
+// LEDs for debug/output
+#include "leds/leds_port.c"
+
+// Include logic for Xilinx Memory Interface Generator w/ AXI interface
+#include "axi_xil_mem.c" 
+
+// Example memory test that writes a test pattern
+// and then reads the same data back
+#define TEST_DATA_SIZE 4 // Single 32b AXI word
+#define TEST_ADDR_MAX (XIL_MEM_ADDR_MAX-TEST_DATA_SIZE+1)
+typedef enum mem_test_state_t
+{
+  WAIT_RESET,
+  WRITES,
+  READS,
+  FAILED
+}mem_test_state_t;
+
+// The memory test process, same clock as generated memory interface
+MAIN_MHZ(app, XIL_MEM_MHZ)
+void app()
+{
+  // State registers
+  static mem_test_state_t mem_test_state;
+  static axi_addr_t test_addr;
+  static uint32_t test_data;
+
+  // Output wire into memory controller
+  app_to_xil_mem = NULL_APP_TO_XIL_MEM;
+  
+  // Next values
+  axi_addr_t next_addr = test_addr + TEST_DATA_SIZE;
+  uint32_t next_test_data = test_data + 1;
+  
+  // MEM TEST FSM
+  if(mem_test_state==WAIT_RESET)
+  {
+    // Wait for DDR reset+calibration to be done
+    uint1_t mem_rst_done
+      = !xil_mem_to_app.ui_clk_sync_rst & xil_mem_to_app.init_calib_complete;
+    if(mem_rst_done)
+    {
+      // Start test
+      mem_test_state = WRITES;
+      test_addr = 0;
+      test_data = 0;
+    }
+    leds = 0;
+  }
+  else if(mem_test_state==WRITES)
+  {
+    // Use logic for AXI write of single 32b word 
+    // (slow, not burst and waits for response)
+    axi32_write_logic_outputs_t axi32_write_logic_outputs 
+      = axi32_write_logic(test_addr, test_data, 1, xil_mem_to_app.axi_dev_to_host);
+    // Write logic drives AXI bus
+    app_to_xil_mem.axi_host_to_dev = axi32_write_logic_outputs.to_dev;
+    // Wait for write to finish
+    if(axi32_write_logic_outputs.done)
+    {
+      // Update test data for next iter
+      test_data = next_test_data;
+      if(test_addr < TEST_ADDR_MAX)
+      {
+        // Next test address
+        test_addr = next_addr;
+      }
+      else
+      {
+        // Done with address space
+        // Begin test of reads
+        test_addr = 0;
+        mem_test_state = READS;
+      }
+    }
+    leds = 0b0011;
+  }
+  else if(mem_test_state==READS)
+  {
+    // Use logic for AXI read of single 32b word 
+    // (slow, not burst and waits for response)
+    axi32_read_logic_outputs_t axi32_read_logic_outputs 
+      = axi32_read_logic(test_addr, 1, xil_mem_to_app.axi_dev_to_host);
+    // Read logic drives AXI bus
+    app_to_xil_mem.axi_host_to_dev = axi32_read_logic_outputs.to_dev;
+    // Wait for read to finish
+    if(axi32_read_logic_outputs.done)
+    {
+      // Compare read data vs expected test data
+      if(axi32_read_logic_outputs.data == test_data)
+      {
+        // Good compare, update test data for next iter
+        test_data = next_test_data;
+        if(test_addr < TEST_ADDR_MAX)
+        {
+          // Next test address
+          test_addr = next_addr;
+        }
+        else
+        {
+          // Done with test, passed, repeat test
+          mem_test_state = WAIT_RESET;
+        }
+      }
+      else
+      {
+        // Bad compare, test failed, stop
+        mem_test_state = FAILED;
+      }
+    }
+  }
+  else
+  {
+    // FAILED
+    leds = 0b1111;
+  }
+  
+  // Resets
+  if(xil_mem_to_app.ui_clk_sync_rst)
+  {
+    mem_test_state = WAIT_RESET;
+  }
+}
