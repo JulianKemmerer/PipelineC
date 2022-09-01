0da62346aa0655e616886327460fa14b9da81020
Zachary Curtis <zack.curtis@me.com>
2022-09-01 02:37:28 +0000

    removed lexer file

--- src/lexer/test_lexer.py
+++
@@ -1,9 +0,0 @@
-from hypothesis import given
-from hypothesis.strategies import text
-
-from lexer import split_lines
-
-
-@given(text())
-def test_split_lines(s):
-    assert len(split_lines(s)) == len(s.split("\n"))
