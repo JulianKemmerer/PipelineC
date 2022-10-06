c549ccfde4bbb14721d2c3c89865c1be3c567eca
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-10-06 00:04:06 -0400

    ram.h Add sim only logic for filtering out invalid comb. read address signals

--- ram.h
+++ ram.h
@@ -30,19 +30,36 @@ ram_name##_outputs_t ram_name( \
   constant SIZE : integer := " xstr(SIZE) "; \n\
   type ram_t is array(0 to SIZE-1) of " xstr(elem_t) "; \n\
   signal the_ram : ram_t := " VHDL_INIT "; \n\
+  -- Limit zero latency comb. read addr range to SIZE \n\
+  -- since invalid addresses can occur as logic propogates \n\
+  -- (this includes out of int32 range u32 values) \n\
+  signal addr0_s : integer range 0 to SIZE-1 := 0; \n\
+  signal addr1_s : integer range 0 to SIZE-1 := 0; \n\
 begin \n\
-  process(clk) \n\
+  process(all) begin \n\
+    addr0_s <= to_integer(addr0(30 downto 0)) \n\
+    -- synthesis translate_off \n\
+    mod SIZE \n\
+    -- synthesis translate_on \n\
+    ; \n\
+    addr1_s <= to_integer(addr1(30 downto 0)) \n\
+    -- synthesis translate_off \n\
+    mod SIZE \n\
+    -- synthesis translate_on \n\
+    ; \n\
+  end process; \n\
+  process(clk) is \n\
   begin \n\
     if rising_edge(clk) then \n\
       if CLOCK_ENABLE(0)='1' then \n\
         if wr_en0(0) = '1' then \n\
-          the_ram(to_integer(addr0)) <= wr_data0; \n\
+          the_ram(addr0_s) <= wr_data0; \n\
         end if; \n\
       end if; \n\
     end if; \n\
   end process; \n\
-  return_output.rd_data0 <= the_ram(to_integer(addr0)); \n\
-  return_output.rd_data1 <= the_ram(to_integer(addr1)); \n\
+  return_output.rd_data0 <= the_ram(addr0_s); \n\
+  return_output.rd_data1 <= the_ram(addr1_s); \n\
 "); \
 }
 
@@ -67,7 +84,24 @@ ram_name##_out_t ram_name( \
   constant SIZE : integer := " xstr(SIZE) "; \n\
   type ram_t is array(0 to SIZE-1) of " xstr(elem_t) "; \n\
   signal the_ram : ram_t := " VHDL_INIT "; \n\
+  -- Limit zero latency comb. read addr range to SIZE \n\
+  -- since invalid addresses can occur as logic propogates \n\
+  -- (this includes out of int32 range u32 values) \n\
+  signal rd_addr0_s : integer range 0 to SIZE-1 := 0; \n\
+  signal rd_addr1_s : integer range 0 to SIZE-1 := 0; \n\
 begin \n\
+  process(all) begin \n\
+    rd_addr0_s <= to_integer(rd_addr0(30 downto 0)) \n\
+    -- synthesis translate_off \n\
+    mod SIZE \n\
+    -- synthesis translate_on \n\
+    ; \n\
+    rd_addr1_s <= to_integer(rd_addr1(30 downto 0)) \n\
+    -- synthesis translate_off \n\
+    mod SIZE \n\
+    -- synthesis translate_on \n\
+    ; \n\
+  end process; \n\
   process(clk) \n\
   begin \n\
     if rising_edge(clk) then \n\
@@ -78,7 +112,7 @@ begin \n\
       end if; \n\
     end if; \n\
   end process; \n\
-  return_output.rd_data0 <= the_ram(to_integer(rd_addr0)); \n\
-  return_output.rd_data1 <= the_ram(to_integer(rd_addr1)); \n\
+  return_output.rd_data0 <= the_ram(rd_addr0_s); \n\
+  return_output.rd_data1 <= the_ram(rd_addr1_s); \n\
 "); \
 }
