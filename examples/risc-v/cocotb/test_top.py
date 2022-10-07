40ba31c99b1b930deeaa50ef35968dd717a9530d
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-10-06 23:43:19 -0400

    RISCV setup cocotb testbench to run cpu to halt or unknown instruction

--- examples/risc-v/cocotb/test_top.py
+++ examples/risc-v/cocotb/test_top.py
@@ -4,22 +4,29 @@ from cocotb.triggers import Timer
 
 from pipelinec_cocotb import * # Generated
 
+def check_cpu_debug(dut):
+    DUMP_PIPELINEC_DEBUG(dut)
+    if halt(dut) == 1:
+        print(f"CPU Halted: returned {int(main_return(dut))}")
+        raise cocotb.result.TestSuccess("RISC-V Test Passed!")
+    assert unknown_op(dut) == 0, "Unknown Instruction!"
+
 @cocotb.test()
-async def my_first_test(dut):
-    """Try accessing the design."""
+async def run_cpu(dut):
     # Do first cycle print a little different
     # to work around 'metavalue detected' warnings from ieee libs
     cycle = 0
-    print("Clock: ", cycle, flush=True)
-    DUMP_PIPELINEC_DEBUG(dut)
+    print("Clock:", cycle, flush=True)
     dut.clk_None.value = 1
     await Timer(0.5, units="ns")
-    print("^End Clock: ", cycle, flush=True)
-    for i in range(139):
+    check_cpu_debug(dut)
+    print("^End Clock:", cycle, flush=True)
+    while True:
         dut.clk_None.value = 0
         await Timer(0.5, units="ns")
         print("")
-        print("Clock: ", i+1, flush=True)
-        DUMP_PIPELINEC_DEBUG(dut)
+        print("Clock:", cycle+1, flush=True)
         dut.clk_None.value = 1
         await Timer(0.5, units="ns")
+        check_cpu_debug(dut)
+        cycle += 1
