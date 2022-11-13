289f7b0ab22eb001e2900bb73f2a6667233f5e56
JulianKemmerer <julian.v.kemmerer@gmail.com>
2022-11-13 14:20:59 -0500

    Better err msg from fsm func parsing confusing

--- src/C_TO_FSM.py
+++ src/C_TO_FSM.py
@@ -1185,6 +1185,8 @@ def C_AST_CTRL_FLOW_FUNC_CALL_TO_STATES(
     # Make intermediate wire+states for things that do, get states
     input_drivers = []
     called_func_name = c_ast_node.name.name
+    if called_func_name not in parser_state.FuncLogicLookupTable:
+        raise Exception(f"No function defintion parsed for: {called_func_name} ? Bad #include order? {c_ast_node.coord}")
     called_func_logic = parser_state.FuncLogicLookupTable[called_func_name]
     for i in range(0, len(called_func_logic.inputs)):
         input_c_ast_node = c_ast_node.args.exprs[i]
