3dae2713335d5c0fb0a34bc6e3c44aeb1f30e39a
JulianKemmerer <julian.v.kemmerer@gmail.com>
2023-04-17 22:46:55 -0400

    Graphics shared bus demo set to 32b wide ram, shorter critical path

--- examples/shared_resource_bus/graphics_demo.c
+++ examples/shared_resource_bus/graphics_demo.c
@@ -6,11 +6,8 @@
 #include "uintN_t.h"
 
 // Next: Make GoL fast...
-//  Cache
-//  Can resize to have as big possible RAM_PIXEL_BUFFER_SIZE
-//    split threads across Y instead of x, like each thread having full line buffers
 //  Try to nudge clocks up for boost
-//
+//     Once over 25Mhz, try less than 16b RAM width and running even faster?
 // FUTURE How to wrap/macro make any new 'resource' (like the frame buffer or autopipeline)
 //
 // Extend to use DDR3 AXI for frame buffer?
@@ -21,10 +18,10 @@
 // NUM_Y_THREADS must evenly divide FRAME_HEIGHT
 // RAM_PIXEL_BUFFER_SIZE is RAM width, number of pixels in x direction
 // Include FRAME_BUF_INIT_DATA header
-#include "frame_buf_init_data_16b.h" // Game of life RAM init data
-#define ram_pixel_offset_t uint4_t
-#define RAM_PIXEL_BUFFER_SIZE_LOG2 4
-#define RAM_PIXEL_BUFFER_SIZE 16
+#include "frame_buf_init_data_32b.h" // Game of life RAM init data
+#define ram_pixel_offset_t uint5_t
+#define RAM_PIXEL_BUFFER_SIZE_LOG2 5
+#define RAM_PIXEL_BUFFER_SIZE 32
 #define DEV_CLK_MHZ 100.0
 #define HOST_CLK_MHZ 25.0
 #define NUM_X_THREADS 4
@@ -219,6 +216,13 @@ void render_frame()
     pixels_kernel_seq_range_INPUT_t fsm_in[NUM_X_THREADS][NUM_Y_THREADS];
     pixels_kernel_seq_range_OUTPUT_t fsm_out[NUM_X_THREADS][NUM_Y_THREADS];
     all_threads_done = 1;
+    for (i = 0; i < NUM_X_THREADS; i+=1)
+    {
+      for (j = 0; j < NUM_Y_THREADS; j+=1)
+      {
+        all_threads_done &= thread_done[i][j];
+      }
+    }
     
     uint16_t THREAD_X_SIZE = FRAME_WIDTH / NUM_X_THREADS;
     uint16_t THREAD_Y_SIZE = FRAME_HEIGHT / NUM_Y_THREADS;
@@ -237,7 +241,7 @@ void render_frame()
           fsm_out[i][j] = pixels_kernel_seq_range_FSM(fsm_in[i][j]);
           thread_done[i][j] = fsm_out[i][j].output_valid;
         }
-        all_threads_done &= thread_done[i][j];
+        //all_threads_done &= thread_done[i][j]; // Longer path
       }
     }
     __clk();
