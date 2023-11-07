da905121c86d74d087c6695ba568434f13d83ef2
JulianKemmerer <julian.v.kemmerer@gmail.com>
2023-11-07 11:13:33 -0500

    Adding hacked template example start

--- examples/constant_arg_variants.c
+++ examples/constant_arg_variants.c
@@ -9,7 +9,7 @@ uint32_t add(uint32_t x, uint32_t c)
 {
   return x + c;
 }
-#pragma MAIN main_add
+//#pragma MAIN main_add
 uint32_t main_add(uint32_t x)
 {
   // Same as template add<1>(x);
@@ -32,7 +32,7 @@ float add_float(float x, float c)
 {
   return x + c;
 }
-#pragma MAIN main_add_float
+//#pragma MAIN main_add_float
 float main_add_float(float x)
 {
   return add_float(x, 1.23);
@@ -48,7 +48,7 @@ uint1_t shift_reg(uint32_t size, uint1_t din, uint1_t enable){
   }
   return rv;
 }
-#pragma MAIN main_shift_reg
+//#pragma MAIN main_shift_reg
 uint1_t main_shift_reg(uint1_t din, uint1_t enable){
   return shift_reg(3, din, enable);
 }
@@ -69,9 +69,43 @@ uint32_t add_const_array(uint32_t x, uint32_t c)
   }
   return a[c];
 }
-#pragma MAIN main_add_const_array
+//#pragma MAIN main_add_const_array
 uint32_t main_add_const_array(uint32_t x)
 {
   return add_const_array(x, 3);
 }
 
+
+#include "xstr.h"
+
+typedef int template_t; // Ignored by PipelineC
+
+#define template_typename(T)\
+typedef template_t T##_t;
+
+#define template(T)\
+T##_t
+
+#define type(T)\
+xstr(T) // Make into string literal
+
+//#define template_func2(func_name, T0, T1, 
+
+template_typename(I)
+template_typename(O)
+template(O) add_type_template(
+  template_t O, template_t I, template(I) x, template(I) y
+){
+  template(O) rv;
+  rv = x + y;
+  return rv;
+}
+#define add_type(out_t, in_t, x, y)\
+add_type_template(type(out_t), type(in_t), x, y)
+
+#define MY_TYPE uint32_t
+#pragma MAIN main_type_add
+MY_TYPE main_type_add(MY_TYPE x, MY_TYPE y)
+{
+  return add_type(MY_TYPE, MY_TYPE, x, y);
+}
