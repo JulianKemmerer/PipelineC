3db9afa9beb0a1e659eb8110fef1ebb6348185ba
JulianKemmerer <julian.v.kemmerer@gmail.com>
2023-12-26 19:54:28 -0500

    example file for trying to improve compare operators

--- examples/operator_experiment.c
+++ examples/operator_experiment.c
@@ -22,14 +22,46 @@ uint1_t main(TEST_LTYPE x, TEST_RTYPE y)
   uint32_t r = ~right+1;
   return left+r;
 }*/
-uint1_t GT(int32_t left, int2_t right)
+
+
+
+
+uint1_t signed_GT_as_sub(int32_t left, int2_t right)
 {
   //int33_t sub = sub_as_adds(right, left);
-  int33_t sub = (int32_t)right - left;
+  int33_t sub = right - left;
   uint1_t lt_zero = sub(32,32);
   return lt_zero;        
 }
+
+// TRY IMPLEMENTING SIGNED COMPARE
+// AS UNSIGNED COMPARE plus sign logic like raw hdl
+//check out better comapres and make signed implemnted as unsigned
+
+
+uint1_t signed_GT_as_unsigned_gt(int32_t left, int2_t right)
+{ 
+  int32_t right_ext = right;
+  uint1_t lsign = left(31,31);
+  uint1_t rsign = right_ext(31,31);
+  uint1_t signs_different = lsign ^ rsign;
+  uint31_t lunsigned = left;
+  uint31_t runsigned = right_ext;
+  // Same sign only compare unsigned magnitude, twos complement makes it make sense
+  uint1_t unsigned_gt = lunsigned > runsigned;
+  uint1_t rv;
+  if(signs_different){
+    rv = rsign;
+  }else{
+    rv = unsigned_gt;
+  }
+  return rv;      
+}
+
 uint1_t main(int32_t x, int2_t y)
 {
-  return GT(x,y); // vs "x>y"
+  return signed_GT_as_unsigned_gt(x,y); // vs "x>y"
 }
+
+
+
