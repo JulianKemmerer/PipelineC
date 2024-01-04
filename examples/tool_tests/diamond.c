b65d583114df2e7e0918547accc7218c824774f4
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-01-04 18:37:04 -0500

    Fix diamond test to use a part diamond tool still supports with free license

--- examples/tool_tests/diamond.c
+++ examples/tool_tests/diamond.c
@@ -1,3 +1,4 @@
-#pragma PART "ICE40UP5K-SG48" // ICE40UP
+//#pragma PART "ICE40UP5K-SG48" // ICE40UP... no longer supported by free Diamond license?
+#pragma PART "LFE5U-45F-8BG381C"// Ex. Hackaday Badge 2019
 #pragma MAIN_MHZ my_pipeline 100.0
 #include "examples/pipeline.c"
