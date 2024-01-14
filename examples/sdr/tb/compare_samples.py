4936666032dfa9599abda479d30ca3d47a442043
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-01-13 21:08:28 -0500

    Update fm radio sim to be more generic and ready to test filters that change sample rate too

--- examples/sdr/tb/compare_samples.py
+++ examples/sdr/tb/compare_samples.py
@@ -7,33 +7,13 @@ from scipy.signal import kaiserord, lfilter, firwin, freqz
 from pylab import figure, clf, plot, xlabel, ylabel, xlim, ylim, title, grid, axes, show
 from os import path
 
-
-#------------------------------------------------
-# Create a signal for demonstration.
-#------------------------------------------------
-
-# For now make same signal on both I(real) and Q(imaginary) channels
-sample_rate = 100.0
-nsamples_in = 250
-t = arange(nsamples_in) / sample_rate
-s = cos(2*pi*0.5*t) + 0.2*sin(2*pi*2.5*t+0.1) + \
-        0.2*sin(2*pi*15.3*t) + 0.1*sin(2*pi*16.7*t + 0.1) + \
-            0.1*sin(2*pi*23.45*t+.8)
-
-s_i = array(s)
-s_q = array(s)
- 
-# Normalize to between -1.0 and +1.0
-s_i /= max(abs(s_i),axis=0)
-s_q /= max(abs(s_q),axis=0)
-
-# Convert float to-from i16
+# Helpers to convert float to-from i16
 i16_max = (2**15)-1
 def to_int16(f):
   return round(f*i16_max).astype(int16)
 def to_float(i16):
   return i16.astype(float)/i16_max
-
+# and how to write C arrays
 def i16_c_array_txt(x, name):
   x_i16 = to_int16(x)
   c_txt = ""
@@ -47,7 +27,24 @@ def i16_c_array_txt(x, name):
       c_txt += "\\\n}\n"
   return c_txt
 
-# Write samples as a C int16 array
+
+#------------------------------------------------
+# Create a signal for demonstration.
+#------------------------------------------------
+sample_rate = 100.0
+nsamples_in = 250
+t = arange(nsamples_in) / sample_rate
+s = cos(2*pi*0.5*t) + 0.2*sin(2*pi*2.5*t+0.1) + \
+        0.2*sin(2*pi*15.3*t) + 0.1*sin(2*pi*16.7*t + 0.1) + \
+            0.1*sin(2*pi*23.45*t+.8)
+
+# Normalize to between -1.0 and +1.0
+s /= max(abs(s),axis=0)
+# For now make same signal on both I(real) and Q(imaginary) channels
+s_i = array(s)
+s_q = array(s)
+
+# Write samples as a C int16 array input for simulation
 f=open("samples.h","w")
 f.write(i16_c_array_txt(s_i,"I_SAMPLES"))
 f.write(i16_c_array_txt(s_q,"Q_SAMPLES"))
@@ -55,38 +52,121 @@ f.close()
 
 
 #------------------------------------------------
-# Create a FIR filter and apply it to s.
+# Create a filter and apply it to s.
 #------------------------------------------------
+#return sample_rate_out,filtered_s_i,filtered_s_q
+def example_fir_dsp(sample_rate, s_i, s_q): 
+  # The Nyquist rate of the signal.
+  nyq_rate = sample_rate / 2.0
 
-# The Nyquist rate of the signal.
-nyq_rate = sample_rate / 2.0
+  # The desired width of the transition from pass to stop,
+  # relative to the Nyquist rate.  We'll design the filter
+  # with a 5 Hz transition width.
+  width = 5.0/nyq_rate
 
-# The desired width of the transition from pass to stop,
-# relative to the Nyquist rate.  We'll design the filter
-# with a 5 Hz transition width.
-width = 5.0/nyq_rate
+  # The desired attenuation in the stop band, in dB.
+  ripple_db = 60.0
 
-# The desired attenuation in the stop band, in dB.
-ripple_db = 60.0
+  # Compute the order and Kaiser parameter for the FIR filter.
+  N, beta = kaiserord(ripple_db, width)
 
-# Compute the order and Kaiser parameter for the FIR filter.
-N, beta = kaiserord(ripple_db, width)
+  # The cutoff frequency of the filter.
+  cutoff_hz = 10.0
 
-# The cutoff frequency of the filter.
-cutoff_hz = 10.0
+  # Use firwin with a Kaiser window to create a lowpass FIR filter.
+  taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
+  #print("taps",taps)
+  #print(i16_c_array_txt(taps, "TAPS"))
+  #print("conv",to_float(to_int16(taps)))
 
-# Use firwin with a Kaiser window to create a lowpass FIR filter.
-taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
-#print("taps",taps)
-#print(i16_c_array_txt(taps, "TAPS"))
-#print("conv",to_float(to_int16(taps)))
+  # Use lfilter to filter s with the FIR filter.
+  filtered_s_i = lfilter(taps, 1.0, s_i)
+  filtered_s_q = lfilter(taps, 1.0, s_q)
+  sample_rate_out = sample_rate # No sample rate change
+  return sample_rate_out,filtered_s_i,filtered_s_q
 
-# Use lfilter to filter s with the FIR filter.
-filtered_s_i = lfilter(taps, 1.0, s_i)
-filtered_s_q = lfilter(taps, 1.0, s_q)
-nsamples_out = nsamples_in
+def decim_5x(sample_rate, s_i, s_q): 
+  taps_int16 = [ -165, \
+  -284, \
+  -219, \
+  -303, \
+  -190, \
+  -102, \
+  98,   \
+  268,  \
+  430,  \
+  482,  \
+  420,  \
+  207,  \
+  -117, \
+  -498, \
+  -835, \
+  -1022,\
+  -957, \
+  -576, \
+  135,  \
+  1122, \
+  2272, \
+  3429, \
+  4419, \
+  5086, \
+  5321, \
+  5086, \
+  4419, \
+  3429, \
+  2272, \
+  1122, \
+  135,  \
+  -576, \
+  -957, \
+  -1022,\
+  -835, \
+  -498, \
+  -117, \
+  207,  \
+  420,  \
+  482,  \
+  430,  \
+  268,  \
+  98,   \
+  -102, \
+  -190, \
+  -303, \
+  -219, \
+  -284, \
+  -165]
+  taps_int16 = array(taps_int16)
+  taps = to_float(taps_int16)
+ 
+  # Use lfilter to filter s with the FIR filter.
+  filtered_s_i = lfilter(taps, 1.0, s_i)
+  filtered_s_q = lfilter(taps, 1.0, s_q)
 
-# Read simulation filtered output, start with zeros
+  # Decimate by 5, Drop 4/5 samples
+  decim_fac = 5
+  sample_rate_out = sample_rate/decim_fac
+  dec_filt_s_i = []
+  dec_filt_s_q = []
+  i = decim_fac-1
+  list_filtered_s_i = list(filtered_s_i)
+  list_filtered_s_q = list(filtered_s_q)
+  while i < len(filtered_s_i):
+    # Take sample
+    dec_filt_s_i.append(list_filtered_s_i[i])
+    dec_filt_s_q.append(list_filtered_s_q[i])
+    # Skip samples
+    i += decim_fac
+  return sample_rate_out,array(dec_filt_s_i),array(dec_filt_s_q)
+
+# Get expected output by applying some DSP
+sample_rate_out,filtered_s_i,filtered_s_q = decim_5x(sample_rate, s_i, s_q)
+nsamples_out = min(len(filtered_s_i),len(filtered_s_q))
+t_out = arange(nsamples_out) / sample_rate_out
+
+
+#------------------------------------------------
+# # Read simulation filtered output, start with zeros
+#------------------------------------------------
 sim_filtered_s_i = array([0]*nsamples_out)
 sim_filtered_s_q = array([0]*nsamples_out)
 fname = "sim_output.log"
@@ -119,91 +199,37 @@ else:
   exit(0)
 
 
-#------------------------------------------------
-# Plot the FIR filter coefficients.
-#------------------------------------------------
-"""
-figure(1)
-plot(taps, 'bo-', linewidth=2)
-title('Filter Coefficients (%d taps)' % N)
-grid(True)
-"""
-#------------------------------------------------
-# Plot the magnitude response of the filter.
-#------------------------------------------------
-"""
-figure(2)
-clf()
-w, h = freqz(taps, worN=8000)
-plot((w/pi)*nyq_rate, absolute(h), linewidth=2)
-xlabel('Frequency (Hz)')
-ylabel('Gain')
-title('Frequency Response')
-ylim(-0.05, 1.05)
-grid(True)
 
-# Upper inset plot.
-ax1 = axes([0.42, 0.6, .45, .25])
-plot((w/pi)*nyq_rate, absolute(h), linewidth=2)
-xlim(0,8.0)
-ylim(0.9985, 1.001)
-grid(True)
-
-# Lower inset plot
-ax2 = axes([0.42, 0.25, .45, .25])
-plot((w/pi)*nyq_rate, absolute(h), linewidth=2)
-xlim(12.0, 20.0)
-ylim(0.0, 0.0025)
-grid(True)
-"""
 #------------------------------------------------
 # Plot the original and filtered signals.
 #------------------------------------------------
 
-# The phase delay of the filtered signal.
-delay = 0.5 * (N-1) / sample_rate
-"""
-figure(3)
 # Plot the original signal.
-plot(t, x)
-# Plot the filtered signal, shifted to compensate for the phase delay.
-plot(t-delay, filtered_x, 'r-')
-# Plot just the "good" part of the filtered signal.  The first N-1
-# samples are "corrupted" by the initial conditions.
-plot(t[N-1:]-delay, filtered_x[N-1:], 'g', linewidth=4)
-
-xlabel('t')
-grid(True)
-"""
-
-# NO DELAY?
-delay = 0.0
-# Plot the expected output vs simulation output
 figure(1)
-title('I - Expected (green) vs. Sim (blue)')
-# Plot the filtered signal, shifted to compensate for the phase delay.
-plot(t-delay, filtered_s_i, 'r-')
-# Plot just the "good" part of the filtered signal.  The first N-1
-# samples are "corrupted" by the initial conditions.
-plot(t[N-1:]-delay, filtered_s_i[N-1:], 'g', linewidth=4)
-# Plot the simulated signal, shifted to compensate for the phase delay.
-plot(t-delay, sim_filtered_s_i, 'b')
+title('I - Input')
+plot(t, s_i)
 xlabel('t')
 grid(True)
-
 figure(2)
-title('Q - Expected (green) vs. Sim (blue)')
-# Plot the filtered signal, shifted to compensate for the phase delay.
-plot(t-delay, filtered_s_q, 'r-')
-# Plot just the "good" part of the filtered signal.  The first N-1
-# samples are "corrupted" by the initial conditions.
-plot(t[N-1:]-delay, filtered_s_q[N-1:], 'g', linewidth=4)
-# Plot the simulated signal, shifted to compensate for the phase delay.
-plot(t-delay, sim_filtered_s_q, 'b')
+title('Q - Input')
+plot(t, s_q)
 xlabel('t')
 grid(True)
 
 
+# Plot the expected output vs simulation output
+figure(3)
+title('I - Expected (green) vs. Sim (blue)')
+plot(t_out, filtered_s_i, 'g', linewidth=4)
+plot(t_out, sim_filtered_s_i, 'b')
+xlabel('t')
+grid(True)
+figure(4)
+title('Q - Expected (green) vs. Sim (blue)')
+plot(t_out, filtered_s_q, 'g', linewidth=4)
+plot(t_out, sim_filtered_s_q, 'b')
+xlabel('t')
+grid(True)
 
 
 show()
