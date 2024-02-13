d0453c9b6f0e7a6eb628820daf902d148b6c4202
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-02-12 20:09:37 -0500

    update gowin example to 200MHz

--- examples/tool_tests/gowin.c
+++ examples/tool_tests/gowin.c
@@ -1,6 +1,6 @@
 // speed grade C8 is equivalent to I7 in this context, and resolves to the expected C8/I7
 // for gw_sh not to complain.
 #pragma PART GW2AR-LV18QN88PC8:C
-#pragma MAIN_MHZ my_pipeline 100.0
+#pragma MAIN_MHZ my_pipeline 200.0
 
 #include "examples/pipeline.c"
