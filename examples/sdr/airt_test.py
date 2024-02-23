3af34a5c36dc6204f39ee8af3cb12247c83b9514
Julian Kemmerer <julian@deepwavedigital.com>
2024-02-23 10:25:38 -0500

    sdr demo fix airt_test.py to use CS16 and simply use samples as audio instead of typical interleaved iq. need to fix sample count math to account for counting audio samples not two-sample fake complex data.

--- examples/sdr/airt_test.py
+++ examples/sdr/airt_test.py
@@ -3,7 +3,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import SoapySDR
-from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_S16 
+from SoapySDR import SOAPY_SDR_RX, SOAPY_SDR_CS16 
+import sys
 
 # Fixed hardware info
 AUDIO_SAMPLE_RATE_HZ = 48000
@@ -25,22 +26,27 @@ timeout_us = int(audio_seconds * 1e6) * 2
 # Receive Signal
 ############################################################################################
 #  Initialize the AIR-T receiver using SoapyAIRT
+print("Init radio...")
 sdr = SoapySDR.Device(dict(driver="SoapyAIRT"))       # Create AIR-T instance
 sdr.setSampleRate(SOAPY_SDR_RX, rx_chan, fs)          # Set sample rate
 sdr.setGainMode(SOAPY_SDR_RX, rx_chan, use_agc)       # Set the gain mode
 sdr.setFrequency(SOAPY_SDR_RX, rx_chan, freq)         # Tune the LO
 
 # Create data buffer and start streaming samples to it
+print("Setup stream...")
 rx_buff = np.empty(N, np.int16)                 # Create memory buffer for data stream
-rx_stream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_S16, [rx_chan])  # Setup data stream
+rx_stream = sdr.setupStream(SOAPY_SDR_RX, SOAPY_SDR_CS16, [rx_chan])  # Setup data stream
+print("Activate stream...")
 sdr.activateStream(rx_stream)  # this turns the radio on
 
 # Read the samples from the data buffer
-sr = sdr.readStream(rx_stream, [rx_buff], N, timeoutUs=timeout_us)
+print("Read stream...")
+sr = sdr.readStream(rx_stream, [rx_buff], int(N/2), timeoutUs=timeout_us)
 rc = sr.ret # number of samples read or the error code
-assert rc == N, 'Error Reading Samples from Device (error code = %d)!' % rc
+assert rc == int(N/2), 'Error Reading Samples from Device (error code = %d)!' % rc
 
 # Stop streaming
+print("Stopping...")
 sdr.deactivateStream(rx_stream)
 sdr.closeStream(rx_stream)
 
@@ -49,6 +55,24 @@ sdr.closeStream(rx_stream)
 ############################################################################################
 # Convert shorts (received signal) to floats [-1, 1]
 #s0 = rx_buff.astype(float) / np.power(2.0, rx_bits-1)
+# Temp check counter
+'''
+print("Counter?")
+print(rx_buff[2:10])
+# Sometimes first two samples/first clock is wrong?
+i = 2
+x = rx_buff[i]
+for i in range(2, len(rx_buff)):
+	if rx_buff[i] != x:
+		print("mismatch",i,rx_buff[i],x)
+		sys.exit(-1)
+	if x==-1:
+		x = -32768
+	else:
+		x += 1
+print(f"Got {audio_seconds} seconds of continuous data")
+sys.exit(0)
+'''
 # Write to 16-bit PCM, Mono.
 from scipy.io.wavfile import write
 write("fm_radio_audio.wav", AUDIO_SAMPLE_RATE_HZ, rx_buff)
