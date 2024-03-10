92a51f5036abbc931424e3f265616a6b627f0578
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-03-10 12:14:02 -0400

    more slow binary tree working

--- examples/slow_binary_tree.c
+++ examples/slow_binary_tree.c
@@ -22,12 +22,12 @@ stream(uint8_t) test_3tree(uint8_t x[3], uint1_t valid){
 #include "slow_binary_tree.h"
 
 // Adder tree of u8s
-DECL_STREAM_TYPE(uint8_t)
-#define slow_out_t uint8_t
-#define slow_tree_t uint8_t
-#define slow_in_t uint8_t 
+DECL_STREAM_TYPE(uint16_t)
+#define slow_out_t uint16_t
+#define slow_tree_t uint16_t
+#define slow_in_t uint16_t 
 #define slow_OP +
-#define SLOW_N_INPUTS 15
+#define SLOW_N_INPUTS 255
 // How slow?
 #define slow_II CEIL_DIV(SLOW_N_INPUTS,2) // maximum=least resources=N inputs / 2
 
@@ -35,29 +35,39 @@ DECL_STREAM_TYPE(uint8_t)
 // N inputs = II 
 // II=1 tree of 2 elements
 // do_not_use_slow_funcs_with_II_of_1
-/*DECL_SLOW_BINARY_TREE(
-  tree8, CEIL_DIV(slow_II,8), none,
-  slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, CEIL_DIV(slow_II,4)
-)*/
 // Termination case
-stream(slow_tree_t) tree8(slow_tree_t data[2], uint1_t valid){
+stream(slow_tree_t) term(slow_tree_t data[2], uint1_t valid){
   stream(slow_tree_t) rv;
   rv.data = data[0] slow_OP data[1];
   rv.valid = valid;
   return rv;
 }
-// II=2 tree of 4 elements
+DECL_SLOW_BINARY_TREE(
+  tree64, CEIL_DIV(slow_II,64), term,
+  slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, CEIL_DIV(slow_II,32)
+)
+DECL_SLOW_BINARY_TREE(
+  tree32, CEIL_DIV(slow_II,32), tree64,
+  slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, CEIL_DIV(slow_II,16)
+)
+DECL_SLOW_BINARY_TREE(
+  tree16, CEIL_DIV(slow_II,16), tree32,
+  slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, CEIL_DIV(slow_II,8)
+)
+DECL_SLOW_BINARY_TREE(
+  tree8, CEIL_DIV(slow_II,8), tree16,
+  slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, CEIL_DIV(slow_II,4)
+)
 DECL_SLOW_BINARY_TREE(
   tree4, CEIL_DIV(slow_II,4), tree8,
   slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, CEIL_DIV(slow_II,2)
 )
-// II=4 tree of 8 elements
 DECL_SLOW_BINARY_TREE(
   tree2, CEIL_DIV(slow_II,2), tree4,
   slow_tree_t, slow_tree_t, slow_tree_t, slow_OP, slow_II // div by 1
 )
-// ^ Component faster II/2 trees of II elements
-// Base binary tree, II=n inputs/2=8
+// ^ Component trees decreasing II and number elements(=II to start) by factor elements 2
+// Base binary tree, II=n inputs/2
 DECL_SLOW_BINARY_TREE(
   the_binary_tree, slow_II, tree2,
   slow_out_t, slow_tree_t, slow_in_t, slow_OP, SLOW_N_INPUTS
@@ -68,15 +78,15 @@ void main()
 {
   static uint32_t cycle = 0;
   uint1_t valid = cycle ==0;
-  uint8_t x[SLOW_N_INPUTS];
+  slow_out_t x[SLOW_N_INPUTS];
   uint32_t i;
-  uint8_t correct_sum = 0;
+  slow_out_t correct_sum = 0;
   for ( i = 0; i < SLOW_N_INPUTS; i+=1)
   {
     x[i] = i;
     correct_sum += i;
   }
-  stream(uint8_t) out_stream = the_binary_tree(x, valid);
+  stream(slow_out_t) out_stream = the_binary_tree(x, valid);
   if(out_stream.valid){
     printf("Cycle %d: sum = %d (expected %d)\n", cycle, out_stream.data, correct_sum);
   }
