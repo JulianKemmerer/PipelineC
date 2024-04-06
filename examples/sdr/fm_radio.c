532d2b4e213e6bd9c8d9d77d5b731d9c935b98c6
Julian Kemmerer <julian@deepwavedigital.com>
2024-04-06 17:53:09 -0400

    sdr demo confirm slow fir interp works in hardware. switch over to using stream header and stream(type) types.

--- examples/sdr/fm_radio.c
+++ examples/sdr/fm_radio.c
@@ -14,7 +14,7 @@ DECL_OUTPUT_REG(uint1_t, debug_data_valid)
 // The datapath to be pipelined to meet the clock rate
 // One sample per clock (maximum), ex. 125MHz = 125MSPS
 //#pragma MAIN_MHZ fm_radio_datapath 125.0 // Datapath can be synthesized alone
-i16_stream_t fm_radio_datapath(ci16_stream_t in_sample){
+stream(int16_t) fm_radio_datapath(stream(ci16_t) in_sample){
   // FIR+decimate to reduce sample rate down to ~300KSPS, 3 stages
   in_sample = iq_decim_5x(in_sample);
   in_sample = iq_decim_10x(in_sample);
@@ -28,12 +28,12 @@ i16_stream_t fm_radio_datapath(ci16_stream_t in_sample){
   debug_data_valid = in_sample.valid;
 
   // FM demodulation
-  i16_stream_t out_sample = fm_demodulate(in_sample);
+  stream(int16_t) out_sample = fm_demodulate(in_sample);
 
   // Down sample to audio sample rate with fixed ratio
   // (N times interpolation/M times decimation) resampler
   //  Interpolation:
-  out_sample = interp_24x(out_sample);
+  out_sample = slow_interp_24x(out_sample);
   //  Decimation: 3 stages
   //    Last decimation stage is configured with extra audio low pass 15Khz
   out_sample = decim_5x(out_sample);
@@ -60,8 +60,8 @@ typedef struct u32_stream_t{
   uint32_t data;
   uint1_t valid;
 } u32_stream_t;
-u32_stream_t two_sample_buffer(i16_stream_t in_sample){
-  static i16_stream_t samples[2];
+u32_stream_t two_sample_buffer(stream(int16_t) in_sample){
+  static stream(int16_t) samples[2];
   if(in_sample.valid){
     ARRAY_1SHIFT_INTO_TOP(samples, 2, in_sample)
   }
@@ -83,13 +83,13 @@ DECL_OUTPUT_REG(uint32_t, audio_samples_data)
 DECL_OUTPUT_REG(uint1_t, audio_samples_valid)
 #pragma MAIN_MHZ sdr_wrapper 125.0
 // Meet timing by a larger margin in synthesis to aid place and route
-#pragma MAIN_SYN_MHZ sdr_wrapper 144.0
+#pragma MAIN_SYN_MHZ sdr_wrapper 147.0
 void sdr_wrapper(){
-  ci16_stream_t in_sample = {
+  stream(ci16_t) in_sample = {
     .data = {.real = i_data, .imag = q_data}, 
     .valid = iq_valid
   };
-  i16_stream_t out_sample = fm_radio_datapath(in_sample);
+  stream(int16_t) out_sample = fm_radio_datapath(in_sample);
   // Deserializer to 2 samples wide output
   u32_stream_t out_stream = two_sample_buffer(out_sample);
   audio_samples_data = out_stream.data;
