89c1c8d9f93632e3de043841eb5ff9fc3ee97155
Julian Kemmerer <julian@deepwavedigital.com>
2024-04-06 13:06:35 -0400

    sdr demo confirmed corrected actually slow output from interp fir works in hardware

--- pipelinec/include/dsp/fir_interp.h
+++ pipelinec/include/dsp/fir_interp.h
@@ -29,26 +29,70 @@
 #define FIR_COEFFS      FIR_INTERP_COEFFS
 #include "dsp/fir.h"
 
-// Insert zeros
+// Insert N-1 zeros
 #define fir_interp_insert_n_zeros fir_interp_insert_n_zeros_func(fir_interp_name)
-PRAGMA_MESSAGE(FUNC_WIRES fir_interp_insert_n_zeros) // Not worried about delay of this func
-fir_interp_data_stream_t fir_interp_insert_n_zeros(fir_interp_data_stream_t in_sample){
-  static uint8_t zero_counter;  // TODO size counter?
+fir_interp_data_stream_t fir_interp_insert_n_zeros(fir_interp_data_stream_t in_sample)
+{
+  // Keep counting up to interp ratio
+  static uint8_t interp_ratio_counter;
+  uint1_t interp_ratio_elapsed = interp_ratio_counter==(FIR_INTERP_FACTOR-1);
+  if(interp_ratio_elapsed){
+    interp_ratio_counter = 0;
+  }else{
+    interp_ratio_counter += 1;
+  }
+
+  // Buffer one sample to output it with gap between it and next sample
+  static fir_interp_data_stream_t sample_buf;
+  fir_interp_data_stream_t sample_from_buf;
+  
+  // Output buffer contents once new next sample arrives
+  if(in_sample.valid){
+    // Output
+    sample_from_buf = sample_buf;
+    // Input
+    sample_buf = in_sample;
+  }
+
+  // Insert N-1 zeros at slower rate
+  static uint8_t zero_counter;
+  static uint16_t sample_periods_count;
+  static uint16_t zeros_interp_period_cycles_counter;
   fir_interp_data_stream_t o;
   o.valid = 0;
-  if(in_sample.valid){
+  if(sample_from_buf.valid){
     // restart zeros
     zero_counter = FIR_INTERP_FACTOR - 1;
+    zeros_interp_period_cycles_counter = 0;
     // output real original sample pulse
     o.valid = 1;
-    o.data = in_sample.data;
+    o.data = sample_from_buf.data;
   }else{
-    // output zeros
-    if(zero_counter > 0){
-      o.valid = 1;
-      o.data = 0;
-      zero_counter -= 1;
+    // output several zeros at counted off rate
+    if(zeros_interp_period_cycles_counter>=(sample_periods_count)){ // Removed -1 here
+      zeros_interp_period_cycles_counter = 0;
+      if(zero_counter > 0){
+        o.valid = 1;
+        o.data = 0;
+        zero_counter -= 1;
+      }
+    }else{
+      zeros_interp_period_cycles_counter += 1;
     }
+  }
+
+  // Counting how many periods
+  // Increment period counter or
+  // set output reg + reset when sample arrive
+  static uint16_t periods_since_sample_count;
+  if(in_sample.valid){
+    // New sample uses period count if had good prev sample in buffer
+    if(sample_from_buf.valid){
+      sample_periods_count = periods_since_sample_count - 1; // Extra minus 1 done here
+    }
+    periods_since_sample_count = 0;
+  }else if(interp_ratio_elapsed){
+    periods_since_sample_count += 1;
   }
   return o;
 }
