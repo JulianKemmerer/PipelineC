77a5b9a2a453c8572460423af296c506ba2b0b29
Julian Kemmerer <julian@deepwavedigital.com>
2024-04-11 11:02:38 -0400

    sdr demo did confirm slow fir interp is working in hardware

--- pipelinec/include/dsp/slow_fir_interp.h
+++ pipelinec/include/dsp/slow_fir_interp.h
@@ -1,4 +1,3 @@
-//TODO replace with SLOW_
 #include "compiler.h"
 #include "stream/stream.h"
 #include "uintN_t.h"
@@ -21,26 +20,71 @@
 #define slow_fir_binary_tree_func slow_fir_interp_binary_tree_func
 #include "dsp/slow_fir.h"
 
-// Insert zeros // TODO REPLACE WITH COUNTED OFF AT INTERP RATE VERSION ONCE TESTED SEPARATE
+// Insert N-1 zeros
+// TODO: THIS WAS JUST COPIED FROM fir_interp.h, share better
 #define slow_fir_interp_insert_n_zeros PPCAT(slow_fir_interp_name,_insert_n_zeros)
-PRAGMA_MESSAGE(FUNC_WIRES slow_fir_interp_insert_n_zeros) // Not worried about delay of this func
-stream(slow_fir_interp_data_t) slow_fir_interp_insert_n_zeros(stream(slow_fir_interp_data_t) in_sample){
-  static uint8_t zero_counter;  // TODO size counter?
+stream(slow_fir_interp_data_t) slow_fir_interp_insert_n_zeros(stream(slow_fir_interp_data_t) in_sample)
+{
+  // Keep counting up to interp ratio
+  static uint8_t interp_ratio_counter;
+  uint1_t interp_ratio_elapsed = interp_ratio_counter==(SLOW_FIR_INTERP_FACTOR-1);
+  if(interp_ratio_elapsed){
+    interp_ratio_counter = 0;
+  }else{
+    interp_ratio_counter += 1;
+  }
+
+  // Buffer one sample to output it with gap between it and next sample
+  static stream(slow_fir_interp_data_t) sample_buf;
+  stream(slow_fir_interp_data_t) sample_from_buf;
+  
+  // Output buffer contents once new next sample arrives
+  if(in_sample.valid){
+    // Output
+    sample_from_buf = sample_buf;
+    // Input
+    sample_buf = in_sample;
+  }
+
+  // Insert N-1 zeros at slower rate
+  static uint8_t zero_counter;
+  static uint16_t sample_periods_count;
+  static uint16_t zeros_interp_period_cycles_counter;
   stream(slow_fir_interp_data_t) o;
   o.valid = 0;
-  if(in_sample.valid){
+  if(sample_from_buf.valid){
     // restart zeros
     zero_counter = SLOW_FIR_INTERP_FACTOR - 1;
+    zeros_interp_period_cycles_counter = 0;
     // output real original sample pulse
     o.valid = 1;
-    o.data = in_sample.data;
+    o.data = sample_from_buf.data;
   }else{
-    // output zeros
-    if(zero_counter > 0){
-      o.valid = 1;
-      o.data = 0;
-      zero_counter -= 1;
+    // output several zeros at counted off rate
+    if(zeros_interp_period_cycles_counter>=(sample_periods_count)){ // Removed -1 here
+      zeros_interp_period_cycles_counter = 0;
+      if(zero_counter > 0){
+        o.valid = 1;
+        o.data = 0;
+        zero_counter -= 1;
+      }
+    }else{
+      zeros_interp_period_cycles_counter += 1;
     }
+  }
+
+  // Counting how many periods
+  // Increment period counter or
+  // set output reg + reset when sample arrive
+  static uint16_t periods_since_sample_count;
+  if(in_sample.valid){
+    // New sample uses period count if had good prev sample in buffer
+    if(sample_from_buf.valid){
+      sample_periods_count = periods_since_sample_count - 1; // Extra minus 1 done here
+    }
+    periods_since_sample_count = 0;
+  }else if(interp_ratio_elapsed){
+    periods_since_sample_count += 1;
   }
   return o;
 }
