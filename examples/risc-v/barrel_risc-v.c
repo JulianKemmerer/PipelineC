c1cacfc96f62857e8ac38478a0ec70645ba95adb
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-04-18 23:43:22 -0400

    barrel riscv multiple instances works in hardware, reorg to be less nested loops which is faster to compile too

--- examples/risc-v/barrel_risc-v.c
+++ examples/risc-v/barrel_risc-v.c
@@ -1,6 +1,6 @@
-// Copy or single cycle CPU rewritten ~netlist/multi MAIN graph style
+// Copy of single cycle CPU rewritten ~netlist/multi MAIN graph style
+// Each node in the graph instantiates N copies of barrel components
 // For now has no flow control
-// TODO make multi instances of this barrel
 
 // Need more ports since feedback to mems?
 #pragma PART "xc7a100tcsg324-1"
@@ -58,8 +58,8 @@ riscv_mem_map_mod_out_t(my_mmio_out_t) my_mem_map_module(
 #include "mem_decl.h" // declare my_riscv_mem_out my_riscv_mem() func
 
 // Config threads
-// Mhz               | ~40  ~55 150  160  ...~fmax 400Mhz?
-// Threads(~#stages) | 1    3   9    16      64?
+// Mhz               | ~40  ~55-60 150  160  ...~fmax 400Mhz?
+// Threads(~#stages) | 1    3      9    16      64?
 // LIMITING FACTOR IS LUTRAM
 // CONVERT TO BRAM
 // THEN LIMITING FACTOR BRAM
@@ -68,7 +68,7 @@ riscv_mem_map_mod_out_t(my_mmio_out_t) my_mem_map_module(
 //  MULTI BARRELS!
 #define CPU_CLK_MHZ 50.0
 #define N_THREADS_PER_BARREL 3
-#define N_BARRELS 2 
+#define N_BARRELS 32 
 // Interconnect wires between stages
 typedef struct thread_context_t{
   uint8_t thread_id;
@@ -182,7 +182,7 @@ void thread_starter_fsm(){
     barrel_start_valid[i] = running_threads[i] < N_THREADS_PER_BARREL;
     barrel_start_thread_id[i] = running_threads[i];
     barrel_start_pc[i] = 0;
-    if(barrel_start_ready[i]){
+    if(barrel_start_valid[i] & barrel_start_ready[i]){
       printf("Barrel %d Thread %d: Started!\n", i, barrel_start_thread_id[i]);
       running_threads[i] += 1;
     }
@@ -191,49 +191,62 @@ void thread_starter_fsm(){
 
 
 // Current PC reg + thread ID + valid bit
-// TODO how to stop and start threads?
+typedef struct pc_stage_out_t{
+  thread_context_t outputs;
+  uint1_t start_ready;
+}pc_stage_out_t;
+pc_stage_out_t pc_stage(
+  uint8_t bid, // Barrel ID for sim printing
+  thread_context_t inputs,
+  uint32_t start_thread_id,
+  uint32_t start_pc,
+  uint1_t start_valid
+){
+  // These regs give this pc stage 1 cycle of delay as baseline (not comb pass through)
+  static uint32_t pc_reg;
+  static uint8_t thread_id_reg;
+  static uint1_t thread_valid_reg;
+  pc_stage_out_t o;
+  o.outputs.pc = pc_reg;
+  o.outputs.thread_id = thread_id_reg;
+  o.outputs.thread_valid = thread_valid_reg;
+  if(o.outputs.thread_valid)
+  {
+    printf("Barrel %d Thread %d: PC = 0x%X\n", bid, o.outputs.thread_id, o.outputs.pc);
+  }
+  pc_reg = 0;
+  thread_id_reg = 0;
+  thread_valid_reg = 0;
+  o.start_ready = 0;
+  if(inputs.thread_valid){
+    pc_reg = inputs.next_pc;
+    thread_id_reg = inputs.thread_id;
+    thread_valid_reg = 1;
+  }else{
+    // No next thread input to stage, accept stage
+    o.start_ready = 1;
+    if(start_valid){
+      pc_reg = start_pc;
+      thread_id_reg = start_thread_id;
+      thread_valid_reg = 1;
+    }
+  }
+  return o;
+}
 #pragma MAIN pc_thread_start_stage
 #pragma FUNC_LATENCY pc_thread_start_stage 1
 void pc_thread_start_stage(){
-  // Stage thread context, input from prev stage
-  thread_context_t inputs[N_BARRELS] = pc_inputs;
-  // PC is start of pipelined and does not ned to pass through outputs to inputs
-  thread_context_t outputs[N_BARRELS];
-  //
-  // These regs give this pc stage 1 cycle of delay as baseline (not comb pass through)
-  static uint32_t pc_reg[N_BARRELS];
-  static uint8_t thread_id_reg[N_BARRELS];
-  static uint1_t thread_valid_reg[N_BARRELS];
   uint32_t i;
   for (i = 0; i < N_BARRELS; i+=1)
   {
-    outputs[i].pc = pc_reg[i];
-    outputs[i].thread_id = thread_id_reg[i];
-    outputs[i].thread_valid = thread_valid_reg[i];
-    if(outputs[i].thread_valid)
-    {
-      printf("Barrel %d Thread %d: PC = 0x%X\n", i, outputs[i].thread_id, outputs[i].pc);
-    }
-    pc_reg[i] = 0;
-    thread_id_reg[i] = 0;
-    thread_valid_reg[i] = 0;
-    barrel_start_ready[i] = 0;
-    if(inputs[i].thread_valid){
-      pc_reg[i] = inputs[i].next_pc;
-      thread_id_reg[i] = inputs[i].thread_id;
-      thread_valid_reg[i] = 1;
-    }else{
-      // No next thread input to stage, accept stage
-      barrel_start_ready[i] = 1;
-      if(barrel_start_valid[i]){
-        pc_reg[i] = barrel_start_pc[i];
-        thread_id_reg[i] = barrel_start_thread_id[i];
-        thread_valid_reg[i] = 1;
-      }
-    }
-    //
-    // Output to next stage
-    pc_outputs[i] = outputs[i];
+    pc_stage_out_t pc_stage_out = pc_stage(
+      i,
+      pc_inputs[i],
+      barrel_start_thread_id[i],
+      barrel_start_pc[i],
+      barrel_start_valid[i]);
+    pc_outputs[i] = pc_stage_out.outputs;
+    barrel_start_ready[i] = pc_stage_out.start_ready;
   }
 }
 
@@ -244,68 +257,89 @@ void pc_thread_start_stage(){
 //  Data memory signals are not driven until later
 //  but are used now, requiring FEEDBACK pragma
 //  + memory mapped IO signals
+typedef struct multi_thread_mem_stage_out_t
+{
+  thread_context_t imem_out;
+  thread_context_t dmem_out;
+  riscv_mem_map_outputs_t mem_map_out[N_THREADS_PER_BARREL];
+  uint1_t mem_out_of_range[N_THREADS_PER_BARREL];
+}multi_thread_mem_stage_out_t;
+multi_thread_mem_stage_out_t multi_thread_mem_2_stages(
+  uint8_t bid, // For sim print
+  thread_context_t imem_in,
+  thread_context_t dmem_in,
+  riscv_mem_map_inputs_t mem_map_in[N_THREADS_PER_BARREL]
+){
+  multi_thread_mem_stage_out_t o;
+  // Stage thread context, input from prev stages
+  o.imem_out = imem_in;
+  o.dmem_out = dmem_in;
+  // Each thread has own instances of a shared instruction and data memory
+  // Only current thread mem is enabled
+  // Output from multiple threads is muxed
+  uint32_t instructions[N_THREADS_PER_BARREL];
+  uint32_t mem_rd_datas[N_THREADS_PER_BARREL];
+  uint8_t tid;
+  for(tid = 0; tid < N_THREADS_PER_BARREL; tid+=1)
+  {
+    uint32_t mem_addr;
+    uint32_t mem_wr_data;
+    uint1_t mem_wr_byte_ens[4];
+    uint1_t mem_rd_en;
+    mem_addr = dmem_in.exe.result; // addr always from execute module, not always used
+    mem_wr_data = dmem_in.reg_file_rd_datas.rd_data2;
+    mem_wr_byte_ens = dmem_in.decoded.mem_wr_byte_ens;
+    mem_rd_en = dmem_in.decoded.mem_rd;
+    // gate enables with thread id compare and valid
+    if(!(dmem_in.thread_valid & (dmem_in.thread_id==tid))){
+      mem_wr_byte_ens[0] = 0;
+      mem_wr_byte_ens[1] = 0;
+      mem_wr_byte_ens[2] = 0;
+      mem_wr_byte_ens[3] = 0;
+      mem_rd_en = 0;
+    }
+    if(mem_wr_byte_ens[0]){
+      printf("Barrel %d Thread %d: Write Mem[0x%X] = %d\n", bid, tid, mem_addr, mem_wr_data);
+    }
+    my_riscv_mem_out_t mem_out = my_riscv_mem(
+      imem_in.pc>>2, // Instruction word read address based on PC
+      mem_addr, // Main memory read/write address
+      mem_wr_data, // Main memory write data
+      mem_wr_byte_ens, // Main memory write data byte enables
+      mem_rd_en // Main memory read enable
+      // Optional memory map inputs
+      #ifdef riscv_mem_map_inputs_t
+      , mem_map_in[tid]
+      #endif
+    );
+    instructions[tid] = mem_out.inst;
+    mem_rd_datas[tid] = mem_out.rd_data;
+    if(mem_rd_en){
+      printf("Barrel %d Thread %d: Read Mem[0x%X] = %d\n", bid, tid, mem_addr, mem_out.rd_data);
+    }
+    o.mem_out_of_range[tid] = mem_out.mem_out_of_range;
+    o.mem_map_out[tid] = mem_out.mem_map_outputs;
+  }
+  // Mux output based on current thread
+  o.imem_out.instruction = instructions[imem_in.thread_id];
+  o.dmem_out.mem_rd_data = mem_rd_datas[dmem_in.thread_id];
+  return o;
+}
 #pragma MAIN mem_2_stages
 void mem_2_stages()
 {
-  // Stage thread context, input from prev stages
-  imem_outputs = imem_inputs;
-  dmem_outputs = dmem_inputs;
-  //
-  uint32_t instructions[N_BARRELS][N_THREADS_PER_BARREL];
-  uint32_t mem_rd_datas[N_BARRELS][N_THREADS_PER_BARREL];
-
   uint32_t bid;
   for (bid = 0; bid < N_BARRELS; bid+=1)
   {
-    // Each thread has own instances of a shared instruction and data memory
-    // Only current thread mem is enabled
-    // Output from multiple threads is muxed
-    uint8_t tid;
-    for(tid = 0; tid < N_THREADS_PER_BARREL; tid+=1)
-    {
-      uint32_t mem_addr;
-      uint32_t mem_wr_data;
-      uint1_t mem_wr_byte_ens[4];
-      uint1_t mem_rd_en;
-      mem_addr = dmem_inputs[bid].exe.result; // addr always from execute module, not always used
-      mem_wr_data = dmem_inputs[bid].reg_file_rd_datas.rd_data2;
-      mem_wr_byte_ens = dmem_inputs[bid].decoded.mem_wr_byte_ens;
-      mem_rd_en = dmem_inputs[bid].decoded.mem_rd;
-
-      // gate enables with thread id compare and valid
-      if(!(dmem_inputs[bid].thread_valid & (dmem_inputs[bid].thread_id==tid))){
-        mem_wr_byte_ens[0] = 0;
-        mem_wr_byte_ens[1] = 0;
-        mem_wr_byte_ens[2] = 0;
-        mem_wr_byte_ens[3] = 0;
-        mem_rd_en = 0;
-      }
-
-      if(mem_wr_byte_ens[0]){
-        printf("Barrel %d Thread %d: Write Mem[0x%X] = %d\n", bid, tid, mem_addr, mem_wr_data);
-      }
-      my_riscv_mem_out_t mem_out = my_riscv_mem(
-        imem_inputs[bid].pc>>2, // Instruction word read address based on PC
-        mem_addr, // Main memory read/write address
-        mem_wr_data, // Main memory write data
-        mem_wr_byte_ens, // Main memory write data byte enables
-        mem_rd_en // Main memory read enable
-        // Optional memory map inputs
-        #ifdef riscv_mem_map_inputs_t
-        , mem_map_inputs[bid][tid]
-        #endif
-      );
-      instructions[bid][tid] = mem_out.inst;
-      mem_rd_datas[bid][tid] = mem_out.rd_data;
-      if(mem_rd_en){
-        printf("Barrel %d Thread %d: Read Mem[0x%X] = %d\n", bid, tid, mem_addr, mem_out.rd_data);
-      }
-      mem_out_of_range[bid][tid] = mem_out.mem_out_of_range;
-      mem_map_outputs[bid][tid] = mem_out.mem_map_outputs;
-    }
-    // Mux output based on current thread
-    imem_outputs[bid].instruction = instructions[bid][imem_inputs[bid].thread_id];
-    dmem_outputs[bid].mem_rd_data = mem_rd_datas[bid][dmem_inputs[bid].thread_id];
+    multi_thread_mem_stage_out_t multi_thread_mem_out = multi_thread_mem_2_stages(
+      bid,
+      imem_inputs[bid],
+      dmem_inputs[bid],
+      mem_map_inputs[bid]
+    );
+    imem_outputs[bid] = multi_thread_mem_out.imem_out;
+    dmem_outputs[bid] = multi_thread_mem_out.dmem_out;
+    mem_map_outputs[bid] = multi_thread_mem_out.mem_map_out;
   }
 }
 #ifdef riscv_mem_map_outputs_t
@@ -369,67 +403,88 @@ void decode_stages()
 }
 
 
+// Multiple copies of N thread register files
+typedef struct multi_thread_reg_files_out_t{
+  thread_context_t rd_outputs;
+  thread_context_t wr_outputs;
+}multi_thread_reg_files_out_t;
+multi_thread_reg_files_out_t multi_thread_reg_files(
+  uint8_t bid, // Barrel ID for sim printing
+  thread_context_t rd_inputs,
+  thread_context_t wr_inputs
+){
+  multi_thread_reg_files_out_t o;
+  // Stage thread context, input from prev stages
+  o.rd_outputs = rd_inputs;
+  o.wr_outputs = wr_inputs;
+  // Each thread has own instances of a shared instruction and data memory
+  uint8_t tid;
+  // Only current thread mem is enabled
+  // Output from multiple threads is muxed
+  reg_file_out_t reg_file_rd_datas[N_THREADS_PER_BARREL];
+  for(tid = 0; tid < N_THREADS_PER_BARREL; tid+=1)
+  {
+    uint1_t wr_thread_sel = wr_inputs.thread_valid & (wr_inputs.thread_id==tid);
+    // Register file reads and writes
+    uint5_t reg_wr_addr;
+    uint32_t reg_wr_data;
+    uint1_t reg_wr_en;
+    if(wr_thread_sel){
+      // Reg file write back, drive inputs 
+      reg_wr_en = wr_inputs.decoded.reg_wr;
+      reg_wr_addr = wr_inputs.decoded.dest;
+      reg_wr_data = wr_inputs.exe.result;
+      // Determine data to write back
+      if(wr_inputs.decoded.mem_to_reg){
+        printf("Barrel %d Thread %d: Write RegFile: MemRd->Reg...\n", bid, tid);
+        reg_wr_data = wr_inputs.mem_rd_data;
+      }else if(wr_inputs.decoded.pc_plus4_to_reg){
+        printf("Barrel %d Thread %d: Write RegFile: PC+4->Reg...\n", bid, tid);
+        reg_wr_data = wr_inputs.pc + 4;
+      }else{
+        if(reg_wr_en)
+          printf("Barrel %d Thread %d: Write RegFile: Execute Result->Reg...\n", bid, tid);
+      }
+      if(reg_wr_en){
+        printf("Barrel %d Thread %d: Write RegFile[%d] = %d\n", bid, tid, wr_inputs.decoded.dest, reg_wr_data);
+      }
+    }
+    reg_file_rd_datas[tid] = reg_file(
+      rd_inputs.decoded.src1, // First read port address
+      rd_inputs.decoded.src2, // Second read port address
+      reg_wr_addr, // Write port address
+      reg_wr_data, // Write port data
+      reg_wr_en // Write enable
+    );
+    // Reg read
+    uint1_t rd_thread_sel = rd_inputs.thread_valid & (rd_inputs.thread_id==tid);
+    if(rd_thread_sel){
+      if(rd_inputs.decoded.print_rs1_read){
+        printf("Barrel %d Thread %d: Read RegFile[%d] = %d\n", bid, tid, rd_inputs.decoded.src1, reg_file_rd_datas[tid].rd_data1);
+      }
+      if(rd_inputs.decoded.print_rs2_read){
+        printf("Barrel %d Thread %d: Read RegFile[%d] = %d\n", bid, tid, rd_inputs.decoded.src2, reg_file_rd_datas[tid].rd_data2);
+      }
+    }
+  }
+  // Mux output for selected thread
+  o.rd_outputs.reg_file_rd_datas = reg_file_rd_datas[rd_inputs.thread_id];
+
+  return o;
+}
 #pragma MAIN reg_file_2_stages
 void reg_file_2_stages()
 {
-  // Stage thread context, input from prev stages
-  reg_rd_outputs = reg_rd_inputs;
-  reg_wr_outputs = reg_wr_inputs;
   uint32_t bid;
   for(bid = 0; bid < N_BARRELS; bid+=1)
   {
-    // Each thread has own instances of a shared instruction and data memory
-    uint8_t tid;
-    // Only current thread mem is enabled
-    // Output from multiple threads is muxed
-    reg_file_out_t reg_file_rd_datas[N_THREADS_PER_BARREL];
-    for(tid = 0; tid < N_THREADS_PER_BARREL; tid+=1)
-    {
-      uint1_t wr_thread_sel = reg_wr_inputs[bid].thread_valid & (reg_wr_inputs[bid].thread_id==tid);
-      // Register file reads and writes
-      uint5_t reg_wr_addr;
-      uint32_t reg_wr_data;
-      uint1_t reg_wr_en;
-      if(wr_thread_sel){
-        // Reg file write back, drive inputs 
-        reg_wr_en = reg_wr_inputs[bid].decoded.reg_wr;
-        reg_wr_addr = reg_wr_inputs[bid].decoded.dest;
-        reg_wr_data = reg_wr_inputs[bid].exe.result;
-        // Determine data to write back
-        if(reg_wr_inputs[bid].decoded.mem_to_reg){
-          printf("Barrel %d Thread %d: Write RegFile: MemRd->Reg...\n", bid, tid);
-          reg_wr_data = reg_wr_inputs[bid].mem_rd_data;
-        }else if(reg_wr_inputs[bid].decoded.pc_plus4_to_reg){
-          printf("Barrel %d Thread %d: Write RegFile: PC+4->Reg...\n", bid, tid);
-          reg_wr_data = reg_wr_inputs[bid].pc + 4;
-        }else{
-          if(reg_wr_en)
-            printf("Barrel %d Thread %d: Write RegFile: Execute Result->Reg...\n", bid, tid);
-        }
-        if(reg_wr_en){
-          printf("Barrel %d Thread %d: Write RegFile[%d] = %d\n", bid, tid, reg_wr_inputs[bid].decoded.dest, reg_wr_data);
-        }
-      }
-      reg_file_rd_datas[tid] = reg_file(
-        reg_rd_inputs[bid].decoded.src1, // First read port address
-        reg_rd_inputs[bid].decoded.src2, // Second read port address
-        reg_wr_addr, // Write port address
-        reg_wr_data, // Write port data
-        reg_wr_en // Write enable
-      );
-      // Reg read
-      uint1_t rd_thread_sel = reg_rd_inputs[bid].thread_valid & (reg_rd_inputs[bid].thread_id==tid);
-      if(rd_thread_sel){
-        if(reg_rd_inputs[bid].decoded.print_rs1_read){
-          printf("Barrel %d Thread %d: Read RegFile[%d] = %d\n", bid, tid, reg_rd_inputs[bid].decoded.src1, reg_file_rd_datas[tid].rd_data1);
-        }
-        if(reg_rd_inputs[bid].decoded.print_rs2_read){
-          printf("Barrel %d Thread %d: Read RegFile[%d] = %d\n", bid, tid, reg_rd_inputs[bid].decoded.src2, reg_file_rd_datas[tid].rd_data2);
-        }
-      }
-    }
-    // Mux output for selected thread
-    reg_rd_outputs[bid].reg_file_rd_datas = reg_file_rd_datas[reg_rd_inputs[bid].thread_id];
+    multi_thread_reg_files_out_t reg_file_out = multi_thread_reg_files(
+      bid, // Barrel ID for sim printing
+      reg_rd_inputs[bid],
+      reg_wr_inputs[bid]
+    );
+    reg_rd_outputs[bid] = reg_file_out.rd_outputs;
+    reg_wr_outputs[bid] = reg_file_out.wr_outputs;
   }
 }
 
