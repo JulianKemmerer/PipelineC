decdaccddf563a8d17c59bba38d3d3b197f00734
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-08-06 20:06:51 -0400

    Xilinx DDR controller shared bus demo file converted to multi host clock shared bus hard coded for cpu+i2s upcoming demo

--- examples/shared_resource_bus/axi_ddr/axi_xil_mem.c
+++ examples/shared_resource_bus/axi_ddr/axi_xil_mem.c
@@ -5,39 +5,38 @@
 
 // Include types for axi shared bus axi_shared_bus_t
 #include "../axi_shared_bus.h"
-#ifndef SHARED_AXI_XIL_MEM_HOST_CLK_MHZ
-#define SHARED_AXI_XIL_MEM_HOST_CLK_MHZ   XIL_MEM_MHZ
-#endif
-#ifndef SHARED_AXI_XIL_MEM_NUM_THREADS
-#define SHARED_AXI_XIL_MEM_NUM_THREADS   1
-#endif
-// Decl instance of shared bus axi_shared_bus_t
-#define SHARED_RESOURCE_BUS_NAME          axi_xil_mem
-#define SHARED_RESOURCE_BUS_TYPE_NAME     axi_shared_bus_t  
-#define SHARED_RESOURCE_BUS_WR_REQ_TYPE   axi_write_req_t 
-#define SHARED_RESOURCE_BUS_WR_DATA_TYPE  axi_write_data_t
-#define SHARED_RESOURCE_BUS_WR_RESP_TYPE  axi_write_resp_t
-#define SHARED_RESOURCE_BUS_RD_REQ_TYPE   axi_read_req_t
-#define SHARED_RESOURCE_BUS_RD_DATA_TYPE  axi_read_data_t   
-#define SHARED_RESOURCE_BUS_HOST_PORTS    SHARED_AXI_XIL_MEM_NUM_THREADS
-#define SHARED_RESOURCE_BUS_HOST_CLK_MHZ  SHARED_AXI_XIL_MEM_HOST_CLK_MHZ
-#define SHARED_RESOURCE_BUS_DEV_PORTS     1
-#define SHARED_RESOURCE_BUS_DEV_CLK_MHZ   XIL_MEM_MHZ
-#include "shared_resource_bus_decl.h" // TODO make helper axi_shared_resource_bus_decl.h?
+
+// Decl multi host clock domain instance of shared bus axi_shared_bus_t
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_NAME          axi_xil_mem
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_TYPE_NAME     axi_shared_bus_t  
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_WR_REQ_TYPE   axi_write_req_t 
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_WR_DATA_TYPE  axi_write_data_t
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_WR_RESP_TYPE  axi_write_resp_t
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_RD_REQ_TYPE   axi_read_req_t
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_RD_DATA_TYPE  axi_read_data_t
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_DEV_PORTS     1
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_DEV_CLK_MHZ   XIL_MEM_MHZ
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_TOTAL_HOST_PORTS 2
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_HOST0_NAME     cpu
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_HOST0_PORTS    1
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_HOST0_CLK_MHZ  25.0
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_HOST1_NAME     i2s
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_HOST1_PORTS    1
+#define MULTI_HOST_CLK_SHARED_RESOURCE_BUS_HOST1_CLK_MHZ  22.579
+#include "multi_host_clk_shared_resource_bus_decl.h"
 
 // Connect Xilinx MIG example AXI types to shared bus AXI types
 MAIN_MHZ(axi_shared_bus_dev_arb_connect, XIL_MEM_MHZ)
 
 // Sometimes you want an special priority port, typically for reading
 // ex. for streaming read data meeting a frame buffer display rate
-#ifdef AXI_XIL_MEM_RD_PRI_PORT
 // Global wires for the priority read port
 axi_shared_bus_t_host_to_dev_t axi_xil_rd_pri_port_mem_host_to_dev_wire;
 axi_shared_bus_t_dev_to_host_t axi_xil_rd_pri_port_mem_dev_to_host_wire;
 void axi_shared_bus_dev_arb_connect()
 {
   // Arbitrate M hosts to N devs
-  SHARED_BUS_ARB_PIPELINED(axi_shared_bus_t, axi_xil_mem, 1) // Not greedy
+  SHARED_BUS_ARB_PIPELINED(axi_shared_bus_t, axi_xil_mem, 1)
 
   // Middle special 2->1 arb with priority for reads on one port
   axi_shared_bus_t_dev_to_host_t rd_pri_port_arb_dev_to_host_in;
@@ -55,19 +54,6 @@ void axi_shared_bus_dev_arb_connect()
   app_to_xil_mem.axi_host_to_dev = port_ctrl.to_axi32_dev;
   rd_pri_port_arb_dev_to_host_in = port_ctrl.to_host;
 }
-#else
-// Regular arb with no priority ports
-void axi_shared_bus_dev_arb_connect()
-{
-  // Arbitrate M hosts to N devs
-  SHARED_BUS_ARB_PIPELINED(axi_shared_bus_t, axi_xil_mem, 1)
-  // Connect devs to frame buffer ports
-  axi_shared_bus_dev_ctrl_t port_ctrl
-    = axi_shared_bus_dev_ctrl_pipelined(xil_mem_to_app.axi_dev_to_host, axi_xil_mem_from_host[0]);
-  app_to_xil_mem.axi_host_to_dev = port_ctrl.to_axi32_dev;
-  axi_xil_mem_to_host[0] = port_ctrl.to_host;
-}
-#endif
 
 // wrappers around shared_resource_bus.h read() and write() dealing with not-burst request types etc
 uint32_t axi_read(uint32_t addr)
