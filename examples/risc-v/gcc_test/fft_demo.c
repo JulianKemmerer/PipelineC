a8bdfbda3029f49b7fed264057f9bfb0d56cb227
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-08-27 12:22:49 -0400

    riscv is2 fft demo convert algorithm code to switch between fixed and float versions

--- examples/risc-v/gcc_test/fft_demo.c
+++ examples/risc-v/gcc_test/fft_demo.c
@@ -3,23 +3,33 @@
 int main(){
 
     // Create arrays 
-    ci16_t* input = (ci16_t*)malloc(NFFT * sizeof(ci16_t));
-    ci32_t* output = (ci32_t*)malloc(NFFT * sizeof(ci32_t));
+    fft_in_t* input = (fft_in_t*)malloc(NFFT * sizeof(fft_in_t));
+    fft_out_t* output = (fft_out_t*)malloc(NFFT * sizeof(fft_out_t));
     float* output_pwr = (float*)malloc(NFFT * sizeof(float));
 
     // Gen Test signal
-    uint32_t Fs = (INT16_MAX+1) / NFFT; // NFFT/1.0 Hz?
+    #ifdef FFT_TYPE_IS_FIXED
+    uint32_t Fs =  (INT16_MAX+1) / NFFT; // NFFT/1.0 Hz?
+    #endif
+    #ifdef FFT_TYPE_IS_FLOAT
+    float Fs = 1.0 / NFFT;
+    #endif
     for (uint32_t i = 0; i < NFFT; i++)
     {
-        input[i] = exp_taylor(4*i*Fs);
+        input[i] = exp_complex(4*i*Fs);
     }
 
     // print input as float 
     for (uint32_t i = 0; i < NFFT; i++)
     {
+      #ifdef FFT_TYPE_IS_FIXED
       float xi = (float)input[i].real / (float)INT16_MAX;
       float xj = (float)input[i].imag / (float)INT16_MAX;
       printf("i,xi,xj,%d,%f,%f\n", i, xi, xj);
+      #endif
+      #ifdef FFT_TYPE_IS_FLOAT
+      printf("i,xi,xj,%d,%f,%f\n", i, input[i].real, input[i].imag);
+      #endif
     }
 
     // apply fft
@@ -32,8 +42,14 @@ int main(){
         //int fj = i-(NFFT>>1);
         //float re_j = (float)output[j].real / (float)INT16_MAX;
         //float im_j = (float)output[j].imag / (float)INT16_MAX;
+        #ifdef FFT_TYPE_IS_FIXED
         float re = (float)output[i].real / (float)INT16_MAX;
         float im = (float)output[i].imag / (float)INT16_MAX;
+        #endif
+        #ifdef FFT_TYPE_IS_FLOAT
+        float re = output[i].real;
+        float im = output[i].imag;
+        #endif
         float pwr2 = (re*re) + (im*im);
         float pwr = pwr2 > 0 ? sqrtf(pwr2) : 0;
         printf("i,re,im,p,%d,%f,%f,%f\n", i, re, im, pwr);
--- examples/risc-v/gcc_test/fft_demo.h
+++ examples/risc-v/gcc_test/fft_demo.h
@@ -5,6 +5,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Select fft data type
+#define FFT_TYPE_IS_FIXED
+//#define FFT_TYPE_IS_FLOAT
+#ifdef FFT_TYPE_IS_FLOAT
+typedef struct complex_t
+{
+    float real, imag;
+}complex_t;
+#define fft_in_t complex_t
+#define fft_out_t complex_t
+#define ONE_PLUS_0I_INIT {1.0, 0.0} // 1 + 0i
+static inline complex_t exp_complex(float x){
+    complex_t rv;
+    rv.real = cos(x);
+    rv.imag = sin(x);
+    return rv;
+}
+#define EXP_COMPLEX_CONST_ONE 1.0
+static inline complex_t mul_in_out(complex_t a, complex_t b){
+    complex_t rv;
+    rv.real = (a.real* b.real) - (a.imag* b.imag);
+    rv.imag = (a.real* b.imag) + (a.imag* b.real);
+    return rv; 
+}
+static inline complex_t mul_in(complex_t a, complex_t b){
+    return mul_in_out(a,b);
+}
+static inline complex_t mul_out(complex_t a, complex_t b){
+    return mul_in_out(a,b);
+}
+#endif
+
+#ifdef FFT_TYPE_IS_FIXED
 /* Base Types */
 
 /* Q1.15 Fixed point */
@@ -12,14 +45,14 @@ typedef struct ci16_t
 {
     int16_t real, imag;
 }ci16_t;
-
+#define ONE_PLUS_0I_INIT {INT16_MAX, 0} // 1 + 0i
 
 static inline int16_t mul16(int16_t a, int16_t b){
     int16_t rv = ((int16_t)a * (int16_t)b) >> 15;
     return rv;
 }
 
-static inline ci16_t mul_ci16(ci16_t a, ci16_t b){
+static inline ci16_t mul_in(ci16_t a, ci16_t b){
     ci16_t rv;
     rv.real = mul16(a.real, b.real) - mul16(a.imag, b.imag);
     rv.imag = mul16(a.real, b.imag) + mul16(a.imag, b.real);
@@ -41,32 +74,24 @@ typedef struct ci32_t
 {
     int32_t real, imag;
 }ci32_t;
+#define fft_in_t ci16_t
+#define fft_out_t ci32_t
 
 static inline int32_t mul32(int32_t a, int32_t b){
     int32_t rv = ((int64_t)a * (int64_t)b) >> 16;
     return rv;
 }
 
-static inline ci32_t mul_ci32(ci32_t a, ci32_t b){
+static inline ci32_t mul_out(ci32_t a, ci32_t b){
     ci32_t rv;
     rv.real = mul32(a.real, b.real) - mul32(a.imag, b.imag);
     rv.imag = mul32(a.real, b.imag) + mul32(a.imag, b.real);
     return rv;
 }
 
-static inline ci32_t add_ci32(ci32_t a, ci32_t b){
-    ci32_t rv = {a.real + b.real, a.imag + b.imag};
-    return rv;
-}
-
-static inline ci32_t sub_ci32(ci32_t a, ci32_t b){
-    ci32_t rv = {a.real - b.real, a.imag - b.imag};
-    return rv;
-}
-
-static inline ci32_t mul_ci16_ci32(ci16_t a, ci32_t b){
+static inline ci32_t mul_in_out(ci16_t a, ci32_t b){
     ci32_t a_e = {.real = (int32_t)a.real << 1, .imag=(int32_t)a.imag << 1};
-    return mul_ci32(a_e,b);
+    return mul_out(a_e,b);
 }
 
 /* code from https://www.coranac.com/2009/07/sines/ */
@@ -113,10 +138,22 @@ static inline int32_t icos_S4(int32_t x)
 /// Complex exponential e ^ (2 * pi * i * x)
 /// @param x   angle (with 2^15 units/circle)
 /// @return    exp value (CI16)
-static inline ci16_t exp_taylor(int32_t x){
+static inline ci16_t exp_complex(int32_t x){
     ci16_t rv;
     rv.real = icos_S4(x);  // cos first
     rv.imag = isin_S4(x);  // then, sin... fixed bug
+    return rv;
+}
+#define EXP_COMPLEX_CONST_ONE (1<<15) // Use INT16_MAX?
+#endif
+
+static inline fft_out_t add_complex(fft_out_t a, fft_out_t b){
+    fft_out_t rv = {a.real + b.real, a.imag + b.imag};
+    return rv;
+}
+
+static inline fft_out_t sub_complex(fft_out_t a, fft_out_t b){
+    fft_out_t rv = {a.real - b.real, a.imag - b.imag};
     return rv;
 }
 
@@ -150,7 +187,10 @@ static inline uint32_t rev(uint32_t v, const uint32_t N){
 /* Based on https://en.wikipedia.org/wiki/Cooley%E2%80%93Tukey_FFT_algorithm#Data_reordering,_bit_reversal,_and_in-place_algorithms */
 /* Compute Iterative Complex-FFT with N < 2^16 bins */
 // Each bin is SAMPLE_RATE / NUM_POINTS (Hz) wide? TODO what about neg freqencies?
-void compute_fft_cc(ci16_t* input, ci32_t* output, uint32_t N){
+void compute_fft_cc(
+    fft_in_t* input, fft_out_t* output,
+    uint32_t N
+){
     /* Bit-Reverse copy */
     for (uint32_t i = 0; i < N; i++)
     {
@@ -165,7 +205,7 @@ void compute_fft_cc(ci16_t* input, ci32_t* output, uint32_t N){
     {
         int32_t m = 1 << s;
         int32_t m_1_2 = m >> 1;
-        ci16_t omega_m = exp_taylor((1<<15) / m); // div here, sadly... can be precomputed LUT perhaps
+        fft_in_t omega_m = exp_complex(EXP_COMPLEX_CONST_ONE / m); // div here, sadly... can be precomputed LUT perhaps
 
         // Fixed the order here... wrong results before...
         // principle root of nth complex
@@ -173,18 +213,18 @@ void compute_fft_cc(ci16_t* input, ci32_t* output, uint32_t N){
         /* do this in parallel ? */
         for (uint32_t k = 0; k < N; k+=m)
         {
-            ci16_t omega = {INT16_MAX, 0}; // 1 + 0i
+            fft_in_t omega = ONE_PLUS_0I_INIT; 
             for (uint32_t j = 0; j < m_1_2; j++)
             {
                 // t = twiddle factor
-                ci32_t t = mul_ci16_ci32(omega, output[k + j + m_1_2]);
-                ci32_t u = output[k + j];
+                fft_out_t t = mul_in_out(omega, output[k + j + m_1_2]);
+                fft_out_t u = output[k + j];
                 // calculating y[k + j]
-                output[k + j] = add_ci32(u, t);
+                output[k + j] = add_complex(u, t);
                 // calculating y[k+j+n/2]
-                output[k + j + m_1_2] = sub_ci32(u, t);
+                output[k + j + m_1_2] = sub_complex(u, t);
                 // updating omega, basically rotating the phase I guess
-                omega = mul_ci16(omega, omega_m);
+                omega = mul_in(omega, omega_m);
             }
         }
     }  
@@ -202,32 +242,3 @@ void color_screen(int width, int height, float* output_pwr, uint32_t N){
     }
   }
 }
-
-/*int main(){
-
-    // Create arrays 
-    ci16_t* input = (ci16_t*)malloc(NFFT * sizeof(ci16_t));
-    ci32_t* output = (ci32_t*)malloc(NFFT * sizeof(ci32_t));
-
-    // Gen Test signal
-    uint32_t Fs = (INT16_MAX+1) / NFFT;
-    for (uint32_t i = 0; i < NFFT; i++)
-    {
-        input[i] = exp_taylor(16*i*Fs);
-    }
-
-    // apply fft 
-    printf("f,v\n");
-    compute_fft_cc(input, output, NFFT);
-
-    // print results 
-    for (uint32_t i = 0; i < NFFT; i++)
-    {
-        uint32_t j = i < (NFFT>>1) ? (NFFT>>1)+i : i-(NFFT>>1); // FFT SHIFT
-        float re = (float)output[j].real / (float)INT16_MAX; // small typo fix
-        float im = (float)output[j].imag / (float)INT16_MAX;
-        float pwr2 = (re*re) + (im*im);
-        float pwr = pwr2 > 0 ? sqrtf(pwr2) : 0;
-        printf("%d, %f\n",i-(NFFT>>1),pwr); // i-(NFFT>>1) yields -NFFT/2, NFFT/2
-    } 
-}*/
