a822b42567e6fbc2a04e842cdd8a2746495213ef
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-09-06 11:35:44 -0400

    fft demo with 128 samples works too, prep for drawing spectrum faster

--- examples/risc-v/gcc_test/i2s_and_fb_test.c
+++ examples/risc-v/gcc_test/i2s_and_fb_test.c
@@ -40,11 +40,52 @@ void draw_spectrum(int width, int height, float* pwr_bins, uint32_t n_bins){
     drawRect(x_start, y_start, x_end, y_end, 255);
   }
 }
+#define N_BINS (NFFT/2)
+/*
+void draw_spectrum_fast(int width, int height, float* pwr_bins){
+  // Remember the power value from last time to make updates easier
+  static int last_height[N_BINS] = {0};
+  // How wide is each bin in pixels
+  int bin_width = width / N_BINS;
+  // Max FFT value depends on if input is complex tone or not?
+  // (Max=nfft/2)(*2 for complex tone input?)
+  float max_pwr = NFFT/4; // /4 since rarely near max audio to speakers
+  for (size_t b = 0; b < N_BINS; b++)
+  {
+    int x_start = b * bin_width;
+    int x_end = (b+1) * bin_width;
+    int bin_height = (pwr_bins[b]/max_pwr) * height;
+    uint8_t color = 0;
+    int y_start = 0;
+    int y_end = 0;
+    // Calculate bounds for this bin
+    // (recall 0,0 is top left)
+    // Increase rect height or shorten?
+    if(bin_height > last_height[b]){
+      // Increase height, draw white
+      y_start = height - bin_height;
+      y_end = height - last_height[b];
+      color = 255;
+    }else{
+      // Decrease height, draw black
+      y_start = height - last_height[b];
+      y_end =  height - bin_height;
+      color = 0;
+    }
+
+    // Rect with proper y bounds
+    drawRect(x_start, y_start, x_end, y_end, color);
+
+    // Store power for next time
+    last_height[b] = bin_height;
+  }
+}
+*/
 
 void main() {
   //int count = 0;
-  // Clear screen to one color
-  drawRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT, 255);
+  // Clear screen to black
+  drawRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT, 0);
   while(1){
     *LED = 0; //mm_status_regs->i2s_rx_out_desc_overflow;
     
@@ -99,7 +140,7 @@ void main() {
     
     // Print screen coloring results
     // Only use positive freq power in first half of array
-    draw_spectrum(FRAME_WIDTH, FRAME_HEIGHT, fft_output_pwr, NFFT/2);
+    draw_spectrum(FRAME_WIDTH, FRAME_HEIGHT, fft_output_pwr, N_BINS);
    
     //count += 1;
   }
