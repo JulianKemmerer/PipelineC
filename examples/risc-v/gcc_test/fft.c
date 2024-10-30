0c19430c4d2352c12b224a86d6878b92f54c71aa
JulianKemmerer <julian.v.kemmerer@gmail.com>
2024-10-29 22:11:45 -0400

    update fft software algorithm to use same iters as hardware

--- examples/risc-v/gcc_test/fft.c
+++ examples/risc-v/gcc_test/fft.c
@@ -1,7 +1,7 @@
 #include "fft.h"
 
 #ifdef FFT_USE_COMB_LOGIC_HARDWARE
-static inline fft_2pt_out_t fft_2pt_hardware(fft_2pt_w_omega_lut_in_t i){
+static inline fft_2pt_out_t fft_2pt_comb_hardware(fft_2pt_w_omega_lut_in_t i){
   // Write input registers contents
   mm_ctrl_regs->fft_2pt_in = i;
   //(takes just 1 or 2 CPU clock cycle, output ready immediately)
@@ -9,7 +9,7 @@ static inline fft_2pt_out_t fft_2pt_hardware(fft_2pt_w_omega_lut_in_t i){
   return mm_status_regs->fft_2pt_out;
 }
 // Macro version of above that does less loads and stores
-#define fft_2pt_hardware_macro(T, U, S, J)\
+#define fft_2pt_comb_hardware_macro(T, U, S, J)\
 mm_ctrl_regs->fft_2pt_in.t = T;\
 mm_ctrl_regs->fft_2pt_in.u = U;\
 mm_ctrl_regs->fft_2pt_in.s = S;\
@@ -35,18 +35,50 @@ void compute_fft_cc(fft_in_t* input, fft_out_t* output){
     for (uint32_t i = 0; i < N; i++)
     {
         uint32_t ri = rev(i);
-        output[i].real = input[ri].real; // Fix here, swap order
-        output[i].imag = input[ri].imag; // Fix here, swap order
+        output[ri].real = input[i].real;
+        output[ri].imag = input[i].imag;
     }
 
-    /* do this sequentially */
+    // FFT iterations using hardware matching iterator structs
+    fft_iters_t iters = FFT_ITERS_NULL_INIT;
+    while(1){
+        uint32_t s = iters.s;
+        uint32_t k = iters.k;
+        uint32_t j = iters.j;
+        uint32_t m = (uint32_t)1 << s;
+        uint32_t m_1_2 = m >> 1;
+        uint32_t t_index = k + j + m_1_2;
+        uint32_t u_index = k + j;
+        #ifdef FFT_USE_COMB_LOGIC_HARDWARE
+        // Invoke hardware comb logic 2pt butterfly
+        fft_2pt_comb_hardware_macro(output[t_index], output[u_index], s, j);
+        #else
+        // Run comb logic on CPU instead of using hardware
+        fft_2pt_w_omega_lut_in_t fft_in;
+        fft_in.t = output[t_index];
+        fft_in.u = output[u_index];
+        fft_in.s = s;
+        fft_in.j = j;
+        fft_2pt_out_t fft_out = fft_2pt_w_omega_lut(fft_in);
+        output[t_index] = fft_out.t;
+        output[u_index] = fft_out.u;
+        #endif
+        if(last_iter(iters)){
+            //iters = FFT_ITERS_NULL;
+            break;
+        }else{
+            iters = next_iters(iters);
+        }
+    }
+
+    /* // do this sequentially 
     // S butterfly levels
     for (uint32_t s = 1; s < (int)ceil(log2(N) + 1.0); s++)
     {
         int32_t m = 1 << s;
         int32_t m_1_2 = m >> 1;
         // principle root of nth complex
-        /* do this in parallel */
+        // do this in parallel 
         for (uint32_t k = 0; k < N; k+=m)
         {   
             uint32_t t_base_index = k + m_1_2;
@@ -57,7 +89,7 @@ void compute_fft_cc(fft_in_t* input, fft_out_t* output){
                 uint32_t u_index = u_base_index + j;
                 #ifdef FFT_USE_COMB_LOGIC_HARDWARE
                 // Invoke hardware comb logic 2pt butterfly
-                fft_2pt_hardware_macro(output[t_index], output[u_index], s, j);
+                fft_2pt_comb_hardware_macro(output[t_index], output[u_index], s, j);
                 #else
                 // Run comb logic on CPU instead of using hardware
                 fft_2pt_w_omega_lut_in_t fft_in;
@@ -71,7 +103,7 @@ void compute_fft_cc(fft_in_t* input, fft_out_t* output){
                 #endif
             }
         }
-    }  
+    }*/ 
     #endif
 }
 
