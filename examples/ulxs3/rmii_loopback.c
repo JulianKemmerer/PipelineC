0ec17a93818ee59cb3eee3bacba6e16a5a5e07c5
JulianKemmerer <julian.v.kemmerer@gmail.com>
2025-01-30 19:33:08 -0500

    add files for ulxs3 rmii loopback

---
+++ examples/ulxs3/rmii_loopback.c
@@ -0,0 +1,46 @@
+#pragma PART "LFE5UM-85F-6BG381C"
+#include "uintN_t.h"
+#include "compiler.h"
+
+// Configure for RMII PHY module instance 
+// connected to these pins on dev board
+#define GP11_IN
+#define GN11_IN
+#define GP12_IN
+#define GN12_IN
+#define GN9_OUT
+#define GP10_OUT
+#define GN10_OUT
+#define GP13_OUT
+#define GP0_OUT
+#include "board/ulxs3.h"
+#define RMII_TX_EN_WIRE gn10
+#define RMII_TX0_WIRE gp10
+#define RMII_TX1_WIRE gn9
+#define RMII_CRS_DV_WIRE gp12
+#define RMII_RX0_WIRE gn11
+#define RMII_RX1_WIRE gp11
+#define RMII_CLK_WIRE gn12
+#include "net/rmii_wires.c"
+#include "net/rmii_eth_mac.c"
+
+// Loopback uses small fifo to to connect RX to TX
+// TODO fix to be stream(axis8_t) instead of uint8_t
+#include "fifo.h"
+#define ETH_FIFO_DEPTH 128
+FIFO_FWFT(eth_fifo, uint8_t, ETH_FIFO_DEPTH)
+
+MAIN_MHZ(main, RMII_CLK_MHZ)
+void main()
+{
+	uint8_t data_in = eth_rx_mac_axis_out.data.tdata[0];
+	uint1_t wr = eth_rx_mac_axis_out.valid;
+  uint1_t rd = eth_tx_mac_input_ready;
+	eth_fifo_t eth_read = eth_fifo(rd, data_in, wr);
+	eth_tx_mac_axis_in.data.tdata[0] = eth_read.data_out;
+	eth_tx_mac_axis_in.data.tkeep[0] = 1; // only 1 byte, always kept
+	eth_tx_mac_axis_in.data.tlast = eth_read.empty; // TODO fix, not correct
+	eth_tx_mac_axis_in.valid = eth_read.data_out_valid;
+	// No RX ready for input, cant push back on rmii rx mac
+	// eth_rx_mac_axis_out_ready = eth_read.data_in_ready;
+}
---
+++ include/board/ulxs3.h
@@ -0,0 +1,28 @@
+// TODO MORE PINS
+#ifdef GP0_OUT
+DECL_OUTPUT(uint1_t, gp0)
+#endif
+#ifdef GN9_OUT
+DECL_OUTPUT(uint1_t, gn9)
+#endif
+#ifdef GP10_OUT
+DECL_OUTPUT(uint1_t, gp10)
+#endif
+#ifdef GN10_OUT
+DECL_OUTPUT(uint1_t, gn10)
+#endif
+#ifdef GP11_IN
+DECL_INPUT(uint1_t, gp11)
+#endif
+#ifdef GN11_IN
+DECL_INPUT(uint1_t, gn11)
+#endif
+#ifdef GP12_IN
+DECL_INPUT(uint1_t, gp12)
+#endif
+#ifdef GN12_IN
+DECL_INPUT(uint1_t, gn12)
+#endif
+#ifdef GP13_OUT
+DECL_OUTPUT(uint1_t, gp13)
+#endif
