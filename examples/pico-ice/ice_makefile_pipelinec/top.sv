4fd283cbd1decf750476f6c6a352d119bad598a9
JulianKemmerer <julian.v.kemmerer@gmail.com>
2025-05-09 20:55:49 -0400

    pico2 ice add pins 23 and 26 as the default rasp pi uart1, and adjust for 48MHz pll input expected

--- examples/pico-ice/ice_makefile_pipelinec/top.sv
+++ examples/pico-ice/ice_makefile_pipelinec/top.sv
@@ -25,7 +25,9 @@ module top(
   inout ICE_28,
   inout ICE_32,
   inout ICE_36,
-  inout ICE_42
+  inout ICE_42,
+  inout ICE_23,
+  inout ICE_26
 );
   // No 12M input for PICO2_ICE
   // High frequency oscillator
@@ -33,16 +35,16 @@ module top(
   //  0b00 = 48 MHz, 0b01 = 24 MHz,
   //  0b10 = 12 MHz, 0b11 = 6 MHz
   // This config needs to match micropython fpga = ice.fpga(frequency= argument?
-  wire clk_12p0;
-  SB_HFOSC#(.CLKHF_DIV("0b10")) u_hfosc (
+  wire clk_48p0;
+  SB_HFOSC#(.CLKHF_DIV("0b00")) u_hfosc (
     .CLKHFPU(1'b1),
     .CLKHFEN(1'b1),
-    .CLKHF(clk_12p0)
+    .CLKHF(clk_48p0)
   );
   // PLL instance to make a clock based on 12MHz
   wire pll_clk;
   pll pll_inst(
-    .clock_in(clk_12p0),
+    .clock_in(clk_48p0),
     .clock_out(pll_clk),
     .locked()
   );
--- include/board/pico2_ice.h
+++ include/board/pico2_ice.h
@@ -26,20 +26,35 @@ DECL_OUTPUT(uint1_t, ICE_41)
 #include "rgb_led/led_rgb_wires.c"
 
 #ifdef DEFAULT_PI_UART
-// This UART is not hard wired, but is first one used in testing
+// These UARTs are not hard wired, but are first ones used in testing
 // Requires extra micropython code to set up on RaspPi
+/*
+// UART0 as default
 #define ICE_11_OUT
 #define UART_TX_OUT_WIRE ICE_11
 #define ICE_9_IN
 #define UART_RX_IN_WIRE ICE_9
+*/
+// UART1 as default
+#define ICE_23_OUT
+#define UART_TX_OUT_WIRE ICE_23
+#define ICE_26_IN
+#define UART_RX_IN_WIRE ICE_26
 #endif
 
 // GPIO multiple use pins
+
 #ifdef ICE_11_OUT
 DECL_OUTPUT(uint1_t, ICE_11)
 #endif
 #ifdef ICE_9_IN
 DECL_INPUT(uint1_t, ICE_9)
+#endif
+#ifdef ICE_23_OUT
+DECL_OUTPUT(uint1_t, ICE_23)
+#endif
+#ifdef ICE_26_IN
+DECL_INPUT(uint1_t, ICE_26)
 #endif
 
 // Expose named PMOD ports
