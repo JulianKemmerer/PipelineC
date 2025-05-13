79aa7027b85c21f76812e38d5131a2b21f72d43e
JulianKemmerer <julian.v.kemmerer@gmail.com>
2025-05-13 19:51:50 -0400

    riscv demo scale down imem and dmem to 1k for testing in small ice40

--- examples/risc-v/gcc_test/mem_map.h
+++ examples/risc-v/gcc_test/mem_map.h
@@ -14,8 +14,8 @@
 // Needs to match link.ld (TODO how to share variables?)
 #define DMEM_ADDR_BIT_CHECK 30
 #define DMEM_BASE_ADDR ((uint32_t)((uint32_t)1<<DMEM_ADDR_BIT_CHECK))
-#define DMEM_SIZE 32768 // Must be decimal constant since VHDL+C literal
-#define IMEM_SIZE 65536 // Must be decimal constant since VHDL+C literal
+#define DMEM_SIZE 1024 // Must be decimal constant since VHDL+C literal
+#define IMEM_SIZE 1024 // Must be decimal constant since VHDL+C literal
 // Any addresses this high up will be mmio
 #define MEM_MAP_ADDR_BIT_CHECK 31
 #define MEM_MAP_BASE_ADDR ((uint32_t)((uint32_t)1<<MEM_MAP_ADDR_BIT_CHECK))
@@ -31,8 +31,8 @@ typedef struct mm_ctrl_regs_t{
   uint32_t led; // Only 4 bits used, see above note rounding to 32b
 }mm_ctrl_regs_t;
 typedef struct mm_status_regs_t{ 
-  uint32_t button; // Only 4 bits used, see above note rounding to 32b
-  uint32_t cpu_clock;
+  uint32_t unused_button; // Only 4 bits used, see above note rounding to 32b
+  uint32_t unused_cpu_clock;
 }mm_status_regs_t;
 // To-from bytes conversion func
 #ifdef __PIPELINEC__
