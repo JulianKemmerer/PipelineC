addf3bd2d743743b6b5288c4bde7fc5437f9aed1
JulianKemmerer <julian.v.kemmerer@gmail.com>
2026-03-22 11:35:22 -0400

    streamsoc: finer vid param inc, better guards on rect draw bounds

--- examples/stream_soc/gpu/software/device.h
+++ examples/stream_soc/gpu/software/device.h
@@ -1,12 +1,12 @@
 #pragma once
 
 void drawRect(uint32_t start_x, uint32_t start_y, uint32_t end_x, uint32_t end_y, uint8_t color, volatile pixel_t* UNUSED_FB_TODO_REMOVE){
+  if(end_x>FRAME_WIDTH) end_x = FRAME_WIDTH;
+  if(end_y>FRAME_HEIGHT) end_y = FRAME_HEIGHT;
   if(start_x==end_x) return; // Zero width
   if(start_y==end_y) return; // Zero height
-  if(start_x>FRAME_WIDTH) return; // Invalid // TODO >=
-  if(end_x>FRAME_WIDTH) return; // Invalid
-  if(start_y>FRAME_HEIGHT) return; // Invalid // TODO >=
-  if(end_y>FRAME_HEIGHT) return; // Invalid
+  if(start_x>=FRAME_WIDTH) return; // Invalid
+  if(start_y>=FRAME_HEIGHT) return; // Invalid
   pixel_t p = {color, color, color, color};
   draw_rect_t cmd;
   cmd.color = p;
