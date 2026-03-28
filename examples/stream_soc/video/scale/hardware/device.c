5f73060791ad1a7099372e56c9538fc9ba7e8cb1
JulianKemmerer <julian.v.kemmerer@gmail.com>
2026-03-27 22:15:37 -0400

    streamsoc: scale fifo bigger

--- examples/stream_soc/video/scale/hardware/device.c
+++ examples/stream_soc/video/scale/hardware/device.c
@@ -131,7 +131,7 @@ uint1_t scale_video_out_ready; // input
 scale2d_params_t scale_params;
 
 // FIFO to avoid overflow due to scaling outputting more pixels than input
-GLOBAL_STREAM_FIFO(video_t, scale_fifo, 8*1024) // 4k shows overflow when scaled?
+GLOBAL_STREAM_FIFO(video_t, scale_fifo, 16*1024) // 4k shows overflow when scaled? 8k sometimes too?
 
 DECL_OUTPUT(uint1_t, scale_overflow)
 
